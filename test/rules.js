0e622e325412fd54bf3cd2c62b27012b8db3238f
Abdelrahman Awad <logaretm1@outlook.com>
2016-07-30 03:50:03 +0300

    modify rules testing and add more tests for new rules

--- test/rules.js
+++ test/rules.js
@@ -3,10 +3,40 @@ import Validator from './../src/validator';
 
 const validator = new Validator({
     email: 'email',
-    name: 'required',
-    title: 'min:3',
-    content: 'max:10',
-    tags: 'in:1,2,3,5'
+    required: 'required',
+    min: 'min:3',
+    max: 'max:10',
+    in: 'in:1,2,3,5',
+    not_in: 'not_in:1,2,3,5',
+    alpha: 'alpha',
+    alpha_dash: 'alpha_dash',
+    alpha_num: 'alpha_num',
+    numeric: 'numeric'
+});
+
+test('it validates alpha', t => {
+    t.true(validator.validate('alpha', 'abcdefgHijklMnOpqRsTUVwxYZ abc'));
+    t.false(validator.validate('alpha', 'abcdefgHijklMnOpqRsTUVwxYZ abc1'));
+    t.true(validator.validate('alpha', ''));
+});
+
+test('it validates alpha_num', t => {
+    t.true(validator.validate('alpha_num', 'abcdefgHijklMnOpqRsTUVwxYZ abc 123'));
+    t.true(validator.validate('alpha_num', ''));
+    t.false(validator.validate('alpha_num', 'abcdefgHijklMnOpqRsTUVwxYZ -'));
+});
+
+test('it validates alpha_dash', t => {
+    t.true(validator.validate('alpha_dash', 'abcdefgHijklMnOpqRsTUVwxYZ abc 123 _-'));
+    t.true(validator.validate('alpha_dash', 'abcdefgHijklMnOpqRsTUVwxYZ abc'));
+    t.true(validator.validate('alpha_dash', ''));
+    t.false(validator.validate('alpha_dash', 'abcdefgHijklMnOpqRsTUVwxYZ @'));
+});
+
+test('it validates numeric', t => {
+    t.true(validator.validate('numeric', '1234567890'));
+    t.false(validator.validate('numeric', '123abc'));
+    t.true(validator.validate('numeric', ''));
 });
 
 test('it validates emails', t => {
@@ -15,26 +45,34 @@ test('it validates emails', t => {
 });
 
 test('it validates min', t => {
-    t.true(validator.validate('title', 'wow'));
-    t.false(validator.validate('title', 'wo'));
+    t.true(validator.validate('min', 'wow'));
+    t.false(validator.validate('min', 'wo'));
 });
 
 test('it validates max', t => {
-    t.true(validator.validate('content', 'not long'));
-    t.false(validator.validate('content', 'this is some very long text'));
+    t.true(validator.validate('max', 'not long'));
+    t.false(validator.validate('max', 'this is some very long text'));
 });
 
 test('it validates required', t => {
-    t.true(validator.validate('name', 'asjdj'));
-    t.true(validator.validate('name', 0));
-    t.false(validator.validate('name', ''));
-    t.false(validator.validate('name', []));
+    t.true(validator.validate('required', 'asjdj'));
+    t.true(validator.validate('required', 0));
+    t.false(validator.validate('required', ''));
+    t.false(validator.validate('required', []));
 });
 
-test('it validates in array', t => {
-    t.true(validator.validate('tags', 1));
-    t.true(validator.validate('tags', 2));
-    t.true(validator.validate('tags', 3));
-    t.true(validator.validate('tags', 5));
-    t.false(validator.validate('tags', 4));
+test('it validates in', t => {
+    t.true(validator.validate('in', 1));
+    t.true(validator.validate('in', 2));
+    t.true(validator.validate('in', 3));
+    t.true(validator.validate('in', 5));
+    t.false(validator.validate('in', 4));
+});
+
+test('it validates not_in', t => {
+    t.false(validator.validate('not_in', 1));
+    t.false(validator.validate('not_in', 2));
+    t.false(validator.validate('not_in', 3));
+    t.false(validator.validate('not_in', 5));
+    t.true(validator.validate('not_in', 4));
 });
