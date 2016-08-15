15ff5d91bf96ea0d9d74c64f53930ad55bd3b8bd
Abdelrahman Awad <logaretm1@outlook.com>
2016-08-15 06:11:04 +0200

    remove directive references to 'this' for vue 2.0

--- src/directive.js
+++ src/directive.js
@@ -2,63 +2,60 @@ import debounce from './utils/debouncer.js';
 
 const DEFAULT_EVENT_NAME = 'veeValidate';
 
+const onInput = (el, binding, { context }) => () => {
+    context.$validator.validate(name, el.value);
+};
+
+const onFileInput = (el, { modifiers }, { context }) => () => {
+    if (! context.$validator.validate(name, el.files) && modifiers.reject) {
+        // eslint-disable-next-line
+        el.value = '';
+    }
+};
+
+const attachValidatorEvent = (el, binding, vnode) => {
+    let callback;
+    if (binding.expression) {
+        callback = onInput(el, binding, vnode);
+    } else {
+        callback = () => vnode.context.$validator.validate(name, binding.value);
+    }
+
+    vnode.context.$on(DEFAULT_EVENT_NAME, callback);
+};
+
 export default (options) => ({
-    onInput() {
-        this.vm.$validator.validate(this.fieldName, this.el.value);
-    },
-    onFileInput() {
-        if (! this.vm.$validator.validate(this.fieldName, this.el.files)
-        && this.modifiers.reject) {
-            this.el.value = '';
-        }
-    },
-    attachValidatorEvent() {
-        this.validateCallback = this.expression ? () => {
-            this.vm.$validator.validate(this.fieldName, this.value);
-        } : () => {
-            this.handler();
-        };
+    bind(el, { expression, modifiers }, vnode) {
+        const name = expression || el.name;
+        this.vm.$validator.attach(name, el.dataset.rules, el.dataset.as);
 
-        this.vm.$on(DEFAULT_EVENT_NAME, this.validateCallback);
-    },
-    bind() {
-        this.fieldName = this.expression || this.el.name;
-        this.vm.$validator.attach(this.fieldName, this.el.dataset.rules, this.el.dataset.as);
-
-        if (this.expression) {
-            this.attachValidatorEvent();
+        if (expression) {
+            attachValidatorEvent(el, { expression }, vnode);
 
             return;
         }
 
-        const handler = this.el.type === 'file' ? this.onFileInput : this.onInput;
-        this.handles = this.el.type === 'file' ? 'change' : 'input';
+        let handler = el.type === 'file' ? onFileInput(el, { modifiers }, vnode) :
+         onInput(el, {}, vnode);
 
-        const delay = this.el.dataset.delay || options.delay;
-        this.handler = delay ? debounce(handler.bind(this), delay) : handler.bind(this);
-        this.el.addEventListener(this.handles, this.handler);
+        const delay = el.dataset.delay || options.delay;
+        handler = delay ? debounce(handler, delay) : handler;
+        this.el.addEventListener(el.type === 'file' ? 'change' : 'input', handler);
 
         this.attachValidatorEvent();
     },
-    update(value) {
-        if (! this.expression) {
+    update(el, { expression, value, modifiers }, { context }) {
+        if (! expression) {
             return;
         }
 
-        if (this.modifiers.initial) {
-            this.modifiers.initial = false;
+        if (modifiers.initial) {
+            // eslint-disable-next-line
+            modifiers.initial = false;
 
             return;
         }
 
-        this.vm.$validator.validate(this.fieldName, value);
-    },
-    unbind() {
-        if (this.handler) {
-            this.el.removeEventListener(this.handles, this.handler);
-        }
-
-        this.vm.$validator.detach(this.fieldName);
-        this.vm.$off(DEFAULT_EVENT_NAME, this.validateCallback);
+        context.$validator.validate(expression || el.name, value);
     }
 });
