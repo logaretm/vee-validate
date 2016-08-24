b76013122fcb3cd671ac7a1554b1c0f9ad7ce07b
Abdelrahman Awad <logaretm1@outlook.com>
2016-08-24 18:12:40 +0200

    fix edge cases with decimal rule

--- test/rules/decimal.js
+++ test/rules/decimal.js
@@ -1,11 +1,14 @@
 import test from 'ava';
 import validate from './../../src/rules/decimal';
 
-test('it validates required', t => {
+test('it validates numerics with decmial numbers', t => {
     const params = [2];
 
     t.false(validate([]));
+    t.false(validate('a'));
+    t.true(validate(''));
     t.true(validate('11'));
+    t.true(validate('.11'));
     t.true(validate('11.223123818'));
     t.true(validate('11.2', params));
     t.true(validate('11.23', params));
