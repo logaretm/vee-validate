74df2bd95415762f74b7e6fc6f9ff6f06a8ebd6d
Abdelrahman Awad <logaretm1@outlook.com>
2016-08-30 18:12:49 +0200

    clean up dimensions rule from uneeded attributes

--- test/rules/dimensions.js
+++ test/rules/dimensions.js
@@ -8,21 +8,20 @@ test('it validates image dimensions', async t => {
     // Prepares Calls to window and Image objects.
     mocks.dimensionsTest({ width: 150, height: 100 });
 
-    // TODO: maybe add acutal file to be tested properly.
-    let value = await validate([mocks.file('file.jpg', 'image/jpeg', 10)], params);
-    t.true(value[0].valid);
+    let result = await validate([mocks.file('file.jpg', 'image/jpeg', 10)], params);
+    t.true(result[0].valid);
 
     // mock a failing Image, even with the right dimensions.
     mocks.dimensionsTest({ width: 150, height: 100}, true);
-    value = await validate([mocks.file('file.jpg', 'image/jpeg', 10)], params);
-    t.false(value[0].valid);
+    result = await validate([mocks.file('file.jpg', 'image/jpeg', 10)], params);
+    t.false(result[0].valid);
 
     // not an image.
-    value = await validate([mocks.file('file.pdf', 'application/pdf', 10)], params);
-    t.false(value);
+    result = await validate([mocks.file('file.pdf', 'application/pdf', 10)], params);
+    t.false(result);
 
     // wrong dimensions.
     mocks.dimensionsTest({ width: 30, height: 20});
-    value = await validate([mocks.file('file.jpg', 'image/jpeg', 10)], params);
-    t.false(value[0].valid);
+    result = await validate([mocks.file('file.jpg', 'image/jpeg', 10)], params);
+    t.false(result[0].valid);
 });
