0938c30bc255be2f1f7bbc2dcc242c411c308a37
imcvampire <quocanh96@gmail.com>
2016-09-23 10:52:37 +0700

    Better style

--- src/rules/text.js
+++ src/rules/text.js
@@ -1,1 +1,2 @@
-export default (value) => ! Array.isArray(value) && /^[a-zA-Z0-9_&@;"'()?/:,!=\.\-\r\n\s]*$/.test(value);
+export default (value) => ! Array.isArray(value) 
+  && /^[a-zA-Z0-9_&@;"'()?/:,!=\.\-\r\n\s]*$/.test(value);
