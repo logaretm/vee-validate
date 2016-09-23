34a7462d16c1ce3792ae38830020002dd75c9185
Abdelrahman Awad <logaretm1@outlook.com>
2016-09-23 08:03:50 +0200

    remove uneeded check and exclude helpers from coverage

--- src/dictionary.js
+++ src/dictionary.js
@@ -96,10 +96,6 @@ export default class Dictionary
     }
 
     _assign(target) {
-        if (target === undefined || target === null) {
-            throw new TypeError('Cannot convert undefined or null to object');
-        }
-
         const output = Object(target);
         for (let index = 1; index < arguments.length; index++) {
             const source = arguments[index];
