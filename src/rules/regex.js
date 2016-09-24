943de4e17c4a11ca6f98097db34cfcfef7282e6d
Abdelrahman Awad <logaretm1@outlook.com>
2016-09-24 18:35:04 +0200

    allow array syntax for fields validation

--- src/rules/regex.js
+++ src/rules/regex.js
@@ -1,1 +1,7 @@
-export default (value, [regex, ...flags]) => new RegExp(regex, flags).test(String(value));
+export default (value, [regex, ...flags]) => {
+    if (regex instanceof RegExp) {
+        return regex.test(value);
+    }
+
+    return new RegExp(regex, flags).test(String(value));
+};
