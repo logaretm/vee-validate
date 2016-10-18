85057269f85f3d965c9513fb955109b616e8605a
Johann Troendle <johann.troendle@gmail.com>
2016-10-18 13:42:59 -0400

    Removed Proxy()

--- test/fieldBag.js
+++ test/fieldBag.js
@@ -5,22 +5,22 @@ test('it adds fields to the internal bag', t => {
     const bag = new FieldBag();
     bag._add('name');
 
-    t.truthy(bag.fields.name.clean);
+    t.truthy(bag.fields('name').clean);
 });
 
 test('it traps access and returns empty object if field does not exist', t => {
     const bag = new FieldBag();
 
-    t.truthy(bag.fields.name);
+    t.truthy(bag.fields('name'));
 });
 
 test('it removes fields from the internal bag', t => {
     const bag = new FieldBag();
     bag._add('name');
-    t.truthy(bag.fields.name.clean);
+    t.truthy(bag.fields('name').clean);
 
     bag._remove('name');
-    t.falsy(bag.fields.name.clean);
+    t.falsy(bag.fields('name').clean);
 });
 
 test('it sets the dirty flag along with other dependant flags', t => {
@@ -28,14 +28,14 @@ test('it sets the dirty flag along with other dependant flags', t => {
     bag._add('name');
 
     bag._setFlags('name', { dirty: true });
-    t.true(bag.fields.name.dirty);
-    t.false(bag.fields.name.clean);
-    t.true(bag.fields.name.failed);
-    t.false(bag.fields.name.passed);
+    t.true(bag.fields('name').dirty);
+    t.false(bag.fields('name').clean);
+    t.true(bag.fields('name').failed);
+    t.false(bag.fields('name').passed);
 
     bag._setFlags('name', { valid: true });
-    t.false(bag.fields.name.failed);
-    t.true(bag.fields.name.passed);
+    t.false(bag.fields('name').failed);
+    t.true(bag.fields('name').passed);
 });
 
 test('it sets the flag along with other dependant flags', t => {
@@ -43,24 +43,24 @@ test('it sets the flag along with other dependant flags', t => {
     bag._add('name');
 
     bag._setFlags('name', { dirty: true });
-    t.true(bag.fields.name.dirty);
-    t.false(bag.fields.name.clean);
-    t.true(bag.fields.name.failed);
-    t.false(bag.fields.name.passed);
+    t.true(bag.fields('name').dirty);
+    t.false(bag.fields('name').clean);
+    t.true(bag.fields('name').failed);
+    t.false(bag.fields('name').passed);
 
     bag._setFlags('name', { valid: true });
-    t.false(bag.fields.name.failed);
-    t.true(bag.fields.name.passed);
+    t.false(bag.fields('name').failed);
+    t.true(bag.fields('name').passed);
 });
 
 test('it adds the field if the field does not exist when setting flags', t => {
     const bag = new FieldBag();
 
     bag._setFlags('name', { dirty: true });
-    t.true(bag.fields.name.dirty);
+    t.true(bag.fields('name').dirty);
 
     bag._setFlags('name', { valid: true });
-    t.true(bag.fields.name.valid);
+    t.true(bag.fields('name').valid);
 });
 
 test('it does not allow adding new flags', t => {
@@ -68,9 +68,9 @@ test('it does not allow adding new flags', t => {
     bag._add('name');
 
     bag._setFlags('name', { flag: true });
-    t.false(bag.fields.name.dirty);
-    t.true(bag.fields.name.clean);
-    t.false(bag.fields.name.failed);
-    t.false(bag.fields.name.passed);
-    t.falsy(bag.fields.name.flag);
+    t.false(bag.fields('name').dirty);
+    t.true(bag.fields('name').clean);
+    t.false(bag.fields('name').failed);
+    t.false(bag.fields('name').passed);
+    t.falsy(bag.fields('name').flag);
 });
