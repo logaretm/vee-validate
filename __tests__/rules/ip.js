7e1f293c3c1a0e25b4e6e1fd67ead9466543e09e
Abdelrahman Awad <logaretm1@outlook.com>
2016-11-02 04:07:28 +0200

    use validator isIP and use ip version

--- __tests__/rules/ip.js
+++ __tests__/rules/ip.js
@@ -1,20 +1,27 @@
 import validate from './../../src/rules/ip';
 
-const valid = [
-    '192.168.1.1',
-    '255.255.255.255'
-];
-
-const invalid = [
-    '192.168.a.1',
-    '255.255.255.256',
-    '23.a.f.234'
-];
-
 it('validates that the string is a valid ipv4 address', () => {
     // valid.
-    valid.forEach(value => expect(validate(value)).toBe(true));
+    ['192.168.1.1', '255.255.255.255'].forEach(value => expect(validate(value)).toBe(true));
 
     // invalid
-    invalid.forEach(value => expect(validate(value)).toBe(false));
+    ['192.168.a.1', '255.255.255.256', '23.a.f.234'].forEach(value => expect(validate(value)).toBe(false));
+});
+
+it('validates that the string is a valid ipv6 address', () => {
+    // valid.
+    [
+        '::1',
+        '2001:db8:0000:1:1:1:1:1',
+        '::ffff:127.0.0.1'
+    ].forEach(value => expect(validate(value, [6])).toBe(true));
+
+    // invalid
+    [
+        '127.0.0.1',
+        '0.0.0.0',
+        '255.255.255.255',
+        '1.2.3.4',
+        '::ffff:287.0.0.1',
+    ].forEach(value => expect(validate(value, [6])).toBe(false));
 });
