8f7863b150bbace1674a0bc74b41168e4e6278b6
Abdelrahman Awad <logaretm1@outlook.com>
2016-11-23 00:01:26 +0200

    change order of checks for the isObject method and add a test

--- __tests__/utils.js
+++ __tests__/utils.js
@@ -10,5 +10,30 @@ it('gets the data attribute prefixed with the plugin', () => {
 
     expect(helpers.getDataAttribute(el, 'rules')).toBe('required');
     expect(helpers.getDataAttribute(el, 'delay')).toBe('100');
-    expect(helpers.getDataAttribute(el, 'as')).toBe(null);
+    expect(helpers.getDataAttribute(el, 'as')).toBeNull();
+});
+
+it('gets the element scope from the element or from the owning form', () => {
+    document.body.innerHTML =
+        `<div>
+            <input id="el" type="text" name="field" data-vv-scope="scope1">
+        </div>`;
+    let el = document.querySelector('#el');
+    expect(helpers.getScope(el)).toBe('scope1');
+
+    document.body.innerHTML =
+        `<form data-vv-scope="form-scope">
+            <input id="el" type="text" name="field">
+        </form>`;
+    el = document.querySelector('#el');
+    expect(helpers.getScope(el)).toBe('form-scope');
+});
+
+it('checks if a value is an object', () => {
+    const isObject = helpers.isObject;
+
+    expect(isObject(null)).toBe(false);
+    expect(isObject([])).toBe(false);
+    expect(isObject('someval')).toBe(false);
+    expect(isObject({})).toBe(true);
 });
