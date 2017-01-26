06499c03d74bab8b702f1e0f6d8fc8720a5b214d
Justus <jrenton87@gmail.com>
2017-01-25 21:06:16 -0600

    add css classes automatically based on input state with option to toggle on/off and option to customize class names

--- src/directives/validate.js
+++ src/directives/validate.js
@@ -1,19 +1,88 @@
 import ListenerGenerator from '../listeners';
-import { getScope, isObject } from '../utils/helpers';
+import { getScope, isObject, addClass, removeClass } from '../utils/helpers';
 
 const listenersInstances = [];
+
+const defaultClassNames = {
+    touched: 'touched', // the control has been blurred
+    untouched: 'untouched', // the control hasn't been blurred
+    valid: 'valid', // model is valid
+    invalid: 'invalid', // model is invalid
+    pristine: 'pristine', // control has not been interacted with
+    dirty: 'dirty' // control has been interacted with
+};
+
+function addClasses(el, fields, classNames = null) {
+    const field = el.getAttribute('name');
+
+    if (!field) {
+        return;
+    }
+
+    classNames = Object.assign({}, defaultClassNames, classNames);
+
+    const isDirty = fields.dirty(name);
+    const isValid = fields.valid(name);
+
+    if (isDirty) {
+        addClass(el, classNames.touched);
+        removeClass(el, classNames.untouched);
+    } else {
+        addClass(el, classNames.untouched);
+        removeClass(el, classNames.touched);
+    }
+
+    if (isValid) {
+        addClass(el, classNames.valid);
+        removeClass(el, classNames.invalid);
+    } else {
+        addClass(el, classNames.invalid);
+        removeClass(el, classNames.valid);
+    }
+}
+
+function setDirty(el, classNames) {
+    classNames = Object.assign({}, defaultClassNames, classNames);
+
+    addClass(el, classNames.dirty);
+    removeClass(el, classNames.pristine);
+}
+
+function setPristine(el, classNames) {
+    classNames = Object.assign({}, defaultClassNames, classNames);
+
+    addClass(el, classNames.pristine);
+    removeClass(el, classNames.dirty);
+}
 
 export default (options) => ({
     bind(el, binding, vnode) {
         const listener = new ListenerGenerator(el, binding, vnode, options);
         listener.attach();
         listenersInstances.push({ vm: vnode.context, el, instance: listener });
+
+        if (options.enableAutoClasses) {
+            const classNames = options.classNames;
+
+            setPristine(el, classNames);
+
+            el.onfocus = () => {
+                setDirty(el, classNames);
+            };
+            
+            addClasses(el, vnode.context.fields, classNames);
+        }
     },
     update(el, { expression, value, oldValue }, { context }) {
+        if (options.enableAutoClasses) {
+            addClasses(el, context.fields, options.classNames);
+        }
+
         if (! expression || JSON.stringify(value) === JSON.stringify(oldValue)) return;
 
         const holder = listenersInstances.filter(l => l.vm === context && l.el === el)[0];
         const scope = isObject(value) ? (value.scope || getScope(el)) : getScope(el);
+
         context.$validator.updateField(
             holder.instance.fieldName,
             isObject(value) ? value.rules : value,
