ebe710c688c1cd424f1d5cf90573764984477328
Tony <tonyli0513@gmail.com>
2017-02-24 20:55:48 +0800

    Update commit to fit codacy analysis

--- src/validator.js
+++ src/validator.js
@@ -425,7 +425,8 @@ export default class Validator
   _getLocalizedParams(rule, scope = '__global__') {
     if (~ ['after', 'before', 'confirmed'].indexOf(rule.name) &&
         rule.params && rule.params[0]) {
-      if (this.$scopes[scope][rule.params[0]] && this.$scopes[scope][rule.params[0]].name) return [this.$scopes[scope][rule.params[0]].name];
+      const param = this.$scopes[scope][rule.params[0]];
+      if (param && param.name) return [param.name];
       return [dictionary.getAttribute(LOCALE, rule.params[0], rule.params[0])];
     }
 
