b0d97da29d45127346cfd7a7b9915f030b090d05
Daniel Cook <Daniel.Cook@paymentshield.co.uk>
2017-04-10 15:24:27 +0100

    Add inclusivity parameter to date_between rule
    Expose the inclusivity parameter from the momentjs date_between function
    to allow the date_between rule to be right, left or all inclusive

--- test/rules/date_between.js
+++ test/rules/date_between.js
@@ -4,7 +4,7 @@ import date_between from './../../src/plugins/date/date_between'; // eslint-disa
 
 const validate = date_between(moment);
 
-test('checks if a date is between two other dates', t => {
+test('checks if a date is between two other dates - exclusive', t => {
     const format = 'DD/MM/YYYY';
 
     t.true(validate('16/09/2016', ['01/09/2016', '20/09/2016', format]));
@@ -18,4 +18,26 @@ test('fails the valiadation if any date is in incorrect format', t => {
     t.false(validate('09/16/2016', ['01/09/2016', '20/09/2016', format])); // invalid value.
     t.false(validate('16/09/2016', ['199/10/2016', '20/09/2016', format])); // invalid low bound.
     t.false(validate('16/09/2016', ['01/09/2016', '1/15/2016', format])); // invalid upper bound.
+});
+
+test('checks if a date is between two other dates - left inclusive', t => {
+    const format = 'DD/MM/YYYY';
+    
+    t.true(validate('01/09/2016', ['01/09/2016', '20/09/2016', '[)', format]));
+    t.false(validate('20/09/2016', ['01/09/2016', '20/09/2016', '[)', format]));    
+});
+
+test('checks if a date is between two other dates - right inclusive', t => {
+    const format = 'DD/MM/YYYY';
+    
+    t.false(validate('01/09/2016', ['01/09/2016', '20/09/2016', '(]', format]));
+    t.true(validate('20/09/2016', ['01/09/2016', '20/09/2016', '(]', format]));    
+});
+
+
+test('checks if a date is between two other dates - all inclusive', t => {
+    const format = 'DD/MM/YYYY';
+    
+    t.true(validate('01/09/2016', ['01/09/2016', '20/09/2016', '[]', format]));
+    t.true(validate('20/09/2016', ['01/09/2016', '20/09/2016', '[]', format]));    
 });
