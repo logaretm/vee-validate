fe17464ababeac69414b19f20240dd37708d719e
Abdelrahman Awad <logaretm1@gmail.com>
2017-06-05 00:14:45 +0200

    add inclusion messages condition for after and before rules closes #537

--- src/plugins/date/messages.js
+++ src/plugins/date/messages.js
@@ -1,8 +1,8 @@
 /* istanbul ignore next */
 /* eslint-disable max-len */
 export default {
-  after: (field, [target]) => `The ${field} must be after ${target}.`,
-  before: (field, [target]) => `The ${field} must be before ${target}.`,
+  after: (field, [target, inclusion]) => `The ${field} must be after ${inclusion ? 'or equal to ' : ''}${target}.`,
+  before: (field, [target, inclusion]) => `The ${field} must be before ${inclusion ? 'or equal to ' : ''}${target}.`,
   date_between: (field, [min, max]) => `The ${field} must be between ${min} and ${max}.`,
   date_format: (field, [format]) => `The ${field} must be in the format ${format}.`
 };
