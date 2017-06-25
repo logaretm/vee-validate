7d1b7f7a0679d2d81a4143140775fc0c03c38828
Abdelrahman Awad <logaretm1@gmail.com>
2017-06-25 07:38:56 +0200

    fix stack trace in validator exceptions

--- src/exceptions/validatorException.js
+++ src/exceptions/validatorException.js
@@ -1,10 +1,6 @@
 export default class ValidatorException extends Error {
   constructor(...args) {
     super(...args);
-    if (Error.captureStackTrace) {
-      Error.captureStackTrace(this, ValidatorException);
-    }
-
     this.message = `[vee-validate]: ${this.message}`;
   }
 }
