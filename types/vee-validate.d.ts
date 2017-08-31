c9106502cc5fa502fa28db5fd1be67359175aa5f
Abdelrahman Awad <logaretm1@gmail.com>
2017-08-31 15:46:38 +0200

    update typings

--- types/vee-validate.d.ts
+++ types/vee-validate.d.ts
@@ -14,10 +14,11 @@ export class FieldFlags {
 
 export class Field {
     id: string;
-    name: string;    
-    scope: string;    
+    name: string;
+    scope: string;
     flags: FieldFlags;
-    isRequired: boolean;    
+    isRequired: boolean;
+    initial: boolean;
     el: any;
     value: any;
     rules: any;
@@ -52,37 +53,38 @@ export class Validator {
     fields: FieldBag;
     fieldBag: FieldFlagsBag;
     strict: boolean;
+    locale: string;
     readonly dictionary: any;
+    readonly rules: Object;
+    static locale: String;
+    static readonly rules: Object;
+    static readonly dictionary: any;
 
-    constructor(validations: any, options: any);    
-    attach(name: string, checks: string|Object, options?: Object): void;
-    clean(): void;
+    constructor(validations: any, options: any);
+    attach(name: string, checks: string|Object, options?: Object): Field;
+    reset(): void;
     detach(name: string, scope?: string): void;
     extend(name: string, validator: Object|Function): void;
     flag(name: string, flags: Object): void;
-    getErrors(): ErrorBag;
-    getLocale(): string;
-    init(): Validator;
-    installDateTimeValidators(moment: any): void;
-    on(name: string, fieldName: string, scope: string, callback: Function): void;
-    off(name: string, fieldName: string, scope: string): void;
     pause(): Validator;
     remove(name: string): void;
+    update(id: string, diff: Object);
     resume(): Validator;
     setLocale(language?: string): void;
+    localize(language: string, dictionary?: Object) :void;
     setStrictMode(strictMode?: boolean): void;
     updateDictionary(data: Object): void;
-    validate(name: string, value: any, scope?: string, throws?: boolean): Promise<any>;
+    validate(name: string, value: any, scope?: string): Promise<any>;
     validateAll(values?: Object, scope?: string): Promise<any>;
     validateScopes(): Promise<any>;
     static create(validations: Object, options: any): Validator;
     static extend(name: string, validator: Object|Function): void;
-    static installDateTimeValidators(moment: any): any;
     static remove(name: string): void;
     static setLocale(language?: string): void;
     static setStrictMode(strictMode?: boolean): void;
     static updateDictionary(data: any): void;
     static addLocale(local: Object): void;
+    static localize(language: string, dictionary?: Object): void;
 }
 
 export const version: string;
