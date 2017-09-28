cacf082d9f953f3ce6eec836e0e1ed8a6369f6da
Abdelrahman Awad <logaretm1@gmail.com>
2017-09-28 02:05:43 +0200

    test proxy util and add some files to the ignore test list

--- tests/utils.js
+++ tests/utils.js
@@ -366,3 +366,11 @@ test('checks if vee-validate is available globally.', () => {
   };
   expect(i18Utils.isDefinedGlobally()).toBe(true);
 });
+
+test('creates a proxy if available', () => {
+  const obj = {};
+  const handler = {};
+
+  // returns the same object if no proxy is detected.
+  expect(utils.createProxy(obj, handler)  === obj).toBe(false);
+});
