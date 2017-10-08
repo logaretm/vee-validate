8531872d6716509de3197c89faad8cfb54cf54ae
Abdelrahman Awad <logaretm1@gmail.com>
2017-10-08 16:38:53 +0200

    add dictionary flow typing

--- src/core/dictionary.js
+++ src/core/dictionary.js
@@ -1,17 +1,31 @@
 import { isObject, assign } from './utils';
 
 // @flow
+
+type MessageGenerator = (...args: any[]) => string;
+
+type Locale = {
+  messages: Object | { [string]: MessageGenerator },
+  attributes: Object | { [string]: string },
+  custom: Object | { [string]: MessageGenerator },
+  dateFormat: ?string
+};
+
+type Dict = { [string]: Locale };
+
 export default class Dictionary {
-  constructor (dictionary = {}) {
+  container: { [string]: Locale };
+
+  constructor (dictionary?: Object = {}) {
     this.container = {};
     this.merge(dictionary);
   }
 
   hasLocale (locale: string): boolean {
-    return !! this.container[locale];
+    return !!this.container[locale];
   }
 
-  setDateFormat (locale, format) {
+  setDateFormat (locale: string, format: string) {
     if (!this.container[locale]) {
       this.container[locale] = {};
     }
@@ -19,7 +33,7 @@ export default class Dictionary {
     this.container[locale].dateFormat = format;
   }
 
-  getDateFormat (locale) {
+  getDateFormat (locale: string) {
     if (!this.container[locale]) {
       return undefined;
     }
@@ -27,8 +41,8 @@ export default class Dictionary {
     return this.container[locale].dateFormat;
   }
 
-  getMessage (locale, key, fallback) {
-    if (! this.hasMessage(locale, key)) {
+  getMessage (locale: string, key: string, fallback ?: string) {
+    if (!this.hasMessage(locale, key)) {
       return fallback || this._getDefaultMessage(locale);
     }
 
@@ -36,26 +50,22 @@ export default class Dictionary {
   }
 
   /**
-   * Gets a specific message for field. fallsback to the rule message.
-   *
-   * @param {String} locale
-   * @param {String} field
-   * @param {String} key
+   * Gets a specific message for field. falls back to the rule message.
    */
-  getFieldMessage (locale, field, key) {
-    if (! this.hasLocale(locale)) {
+  getFieldMessage (locale: string, field: string, key: string) {
+    if (!this.hasLocale(locale)) {
       return this.getMessage(locale, key);
     }
 
     const dict = this.container[locale].custom && this.container[locale].custom[field];
-    if (! dict || ! dict[key]) {
+    if (!dict || !dict[key]) {
       return this.getMessage(locale, key);
     }
 
     return dict[key];
   }
 
-  _getDefaultMessage (locale) {
+  _getDefaultMessage (locale: string) {
     if (this.hasMessage(locale, '_default')) {
       return this.container[locale].messages._default;
     }
@@ -63,15 +73,15 @@ export default class Dictionary {
     return this.container.en.messages._default;
   }
 
-  getAttribute (locale, key, fallback = '') {
-    if (! this.hasAttribute(locale, key)) {
+  getAttribute (locale: string, key: string, fallback?: string = '') {
+    if (!this.hasAttribute(locale, key)) {
       return fallback;
     }
 
     return this.container[locale].attributes[key];
   }
 
-  hasMessage (locale, key) {
+  hasMessage (locale: string, key: string) {
     return !! (
       this.hasLocale(locale) &&
             this.container[locale].messages &&
@@ -79,7 +89,7 @@ export default class Dictionary {
     );
   }
 
-  hasAttribute (locale, key) {
+  hasAttribute (locale: string, key: string) {
     return !! (
       this.hasLocale(locale) &&
             this.container[locale].attributes &&
@@ -87,11 +97,11 @@ export default class Dictionary {
     );
   }
 
-  merge (dictionary) {
+  merge (dictionary: Dict) {
     this._merge(this.container, dictionary);
   }
 
-  setMessage (locale, key, message) {
+  setMessage (locale: string, key: string, message: MessageGenerator | string) {
     if (! this.hasLocale(locale)) {
       this.container[locale] = {
         messages: {},
@@ -102,7 +112,7 @@ export default class Dictionary {
     this.container[locale].messages[key] = message;
   }
 
-  setAttribute (locale, key, attribute) {
+  setAttribute (locale: string, key: string, attribute: string) {
     if (! this.hasLocale(locale)) {
       this.container[locale] = {
         messages: {},
@@ -113,12 +123,12 @@ export default class Dictionary {
     this.container[locale].attributes[key] = attribute;
   }
 
-  _merge (target, source) {
+  _merge (target: Dict, source: Dict) {
     if (! (isObject(target) && isObject(source))) {
       return target;
     }
 
-    Object.keys(source).forEach(key => {
+    Object.keys(source).forEach((key: string) => {
       if (isObject(source[key])) {
         if (! target[key]) {
           assign(target, { [key]: {} });
