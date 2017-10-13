309515d2ce9cfb8632b90b7bb8c5975c956eb8d2
Sebastian Osorio <sebas212@gmail.com>
2017-10-13 18:31:11 +0200

    Import statements and ComponentOptions changed, in order to support TypeScript Changes in Vue 2.5

--- types/vue.d.ts
+++ types/vue.d.ts
@@ -2,16 +2,16 @@
  * Augment the typings of Vue.js
  */
 
-import Vue = require("vue");
+import Vue, { ComponentOptions } from 'vue';
 import { Validator } from './vee-validate.d';
 
-declare module "vue/types/options" {
+declare module 'vue/types/options' {
   interface ComponentOptions<V extends Vue> {
     $validates?: boolean;
   }
 }
 
-declare module "vue/types/vue" {
+declare module 'vue/types/vue' {
   interface Vue {
     $validator: Validator;
   }
