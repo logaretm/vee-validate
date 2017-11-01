546ee6c23e4620644bbbbd9d9be1d16d74140779
Abdelrahman Awad <logaretm1@gmail.com>
2017-11-01 14:06:15 +0200

    add mixin and directive exports

--- src/index.minimal.js
+++ src/index.minimal.js
@@ -1,4 +1,6 @@
 import install from './install';
+import directive from './directive';
+import mixin from './mixin';
 import use from './use';
 import Validator from './core/validator';
 import mapFields from './core/helpers';
@@ -7,6 +9,8 @@ import ErrorBag from './core/errorBag';
 export default {
   install,
   use,
+  directive,
+  mixin,
   mapFields,
   Validator,
   ErrorBag,
