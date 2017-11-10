77d2834759b3df6eb0751a025236e872479a83b4
Abdelrahman Awad <logaretm1@gmail.com>
2017-11-10 23:30:48 +0200

    add validated flag merge strategy

--- src/core/helpers.js
+++ src/core/helpers.js
@@ -28,7 +28,8 @@ const combine = (lhs: MapObject, rhs: MapObject): boolean => {
     valid: (lhs, rhs) => lhs && rhs,
     invalid: (lhs, rhs) => lhs || rhs,
     pending: (lhs, rhs) => lhs || rhs,
-    required: (lhs, rhs) => lhs || rhs
+    required: (lhs, rhs) => lhs || rhs,
+    validated: (lhs, rhs) => lhs && rhs
   };
 
   return Object.keys(mapper).reduce((flags, flag) => {
