0cacd8cee381e498db8183e397026f817f917812
Abdelrahman Awad <logaretm1@gmail.com>
2017-12-03 12:16:28 +0200

    added is default value test

--- tests/rules/is.js
+++ tests/rules/is.js
@@ -3,4 +3,5 @@ import validate from './../../src/rules/is';
 test('checks if the value matches another', () => {
   expect(validate(1, ['1'])).toBe(false);
   expect(validate(1, [1])).toBe(true);
+  expect(validate(1)).toBe(false);
 });
