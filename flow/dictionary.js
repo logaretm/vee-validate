c70c012b4b8942e6b66b521bdda8af0fa7d288f4
Abdelrahman Awad <logaretm1@gmail.com>
2017-12-05 10:05:58 +0200

    update flow definitions

--- flow/dictionary.js
+++ flow/dictionary.js
@@ -8,3 +8,14 @@ declare type Locale = {
 };
 
 declare type PartialDictionary = { [string]: Locale };
+
+declare interface IDictionary {
+  getMessage (locale: string, key: string, data: any[]): string;
+  setMessage(locale: string, key: string, value: string | MessageGenerator): void;
+  getAttribute(locale: string, key: string): string;
+  setAttribute(locale: string, key: string, value: string): string;
+  getFieldMessage(locale: string, field: string, key: string, data: any[]): string;
+  merge(dictionary: MapObject): void;
+  getDateFormat(locale: string): string | null;
+  setDateFormat(locale: string, format: string): void;
+}
