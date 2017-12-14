b495b43a8a2a0f14634d72fff8154488e6531372
Abdelrahman Awad <logaretm1@gmail.com>
2017-12-15 00:34:33 +0200

    added arg to the model test

--- tests/integration/model.js
+++ tests/integration/model.js
@@ -8,17 +8,35 @@ Vue.use(VeeValidate);
 
 test('watches input value on model change', async () => {
   const wrapper = shallow(TestComponent, { localVue: Vue });
+  expect(wrapper.vm.errors.count()).toBe(0);
+  wrapper.setData({ value: '' });
+  await flushPromises();
+  expect(wrapper.vm.errors.has('field')).toBe(true);
+});
 
+test('model can be watched via the directive arg', async() => {
+  const wrapper = shallow(TestComponent, { localVue: Vue });
+  expect(wrapper.vm.errors.has('argField')).toBe(false);
+  wrapper.setData({ input: '' });
+  await flushPromises();
+  expect(wrapper.vm.errors.has('argField')).toBe(true);
+});
+
+
+
+test('falls back to DOM events if the model is unwatchable', async() => {
+  const wrapper = shallow(TestComponent, { localVue: Vue });
   expect(wrapper.vm.errors.count()).toBe(0);
   wrapper.setData({
-    value: ''
+    form: {
+      value: ''
+    }
   });
 
   await flushPromises();
-
-  expect(wrapper.vm.errors.count()).toBe(1);
-
-  wrapper.setData({
-    value: 'valid'
-  });
+  expect(wrapper.vm.errors.has('unwatchablefield')).toBe(false);
+  wrapper.find('#unwatchable').trigger('input');
+  await flushPromises();
+  expect(wrapper.vm.errors.has('unwatchablefield')).toBe(true);
 });
+
