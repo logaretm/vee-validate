2a557e5bf34a08536923844d70837b51d6182ddc
Abdelrahman Awad <logaretm1@gmail.com>
2017-12-14 17:01:06 +0200

    added scopes test

---
+++ tests/integration/components/Scopes.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <form action="" data-vv-scope="first">
+      <input type="text" v-validate="'required'" name="firstField">
+      <input type="text" v-validate="'required'" name="secondField" data-vv-scope="second">
+    </form>
+
+    <form :data-vv-scope="scope">
+      <input type="text" v-validate="'required'" name="thirdField">
+    </form>
+    <input type="text" v-validate="'required'" name="fourthField" :data-vv-scope="scope">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'scopes-test',
+  data: () => ({
+    scope: 'third'
+  })
+};
+</script>
