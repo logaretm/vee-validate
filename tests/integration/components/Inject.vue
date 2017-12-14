d1a2094da277d686c4dd54c458d7d05ee47033f7
Abdelrahman Awad <logaretm1@gmail.com>
2017-12-14 16:23:15 +0200

    added inject tests

---
+++ tests/integration/components/Inject.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <child-with-inject ref="inject"></child-with-inject>
+    <child-new-inject ref="new"></child-new-inject>
+    <indifferent-child ref="none"></indifferent-child>
+  </div>
+</template>
+
+
+<script>
+import ChildWithInject from './stubs/ChildWithParentValidatorInjection';
+import ChildNewInject from './stubs/ChildWithNewValidatorInjection';
+import IndifferentChild from './stubs/Input';
+
+export default {
+  name: 'inject-test',
+  components: {
+    ChildWithInject,
+    ChildNewInject,
+    IndifferentChild
+  }
+};
+</script>
