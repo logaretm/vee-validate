f4cb14068740c37fb731bf22d27f6dcc495042f7
Abdelrahman Awad <logaretm1@gmail.com>
2017-12-14 20:25:27 +0200

    added validator instance warn when binding the directive

--- tests/integration/lifecycle.js
+++ tests/integration/lifecycle.js
@@ -1,16 +1,24 @@
-import { mount, createLocalVue } from 'vue-test-utils';
+import { mount, shallow, createLocalVue } from 'vue-test-utils';
 import flushPromises from 'flush-promises';
 import VeeValidate from './../../src/index';
-import TestComponent from './components/Inject';
+import BasicComponent from './components/Basic';
+import InjectComponent from './components/Inject';
 import ChildInject from './components/stubs/ChildWithParentValidatorInjection';
 import ChildNew from './components/stubs/ChildWithNewValidatorInjection';
 
+test('warns if no validator instance was found in the directive context on bind', () => {
+  global.console.warn = jest.fn();
+  const Vue = createLocalVue();
+  Vue.directive('validate', VeeValidate.directive);
+  const wrapper = shallow(BasicComponent, { localVue: Vue });
+  expect(global.console.warn).toHaveBeenCalled();
+});
 
 test('the validator instance is destroyed when the owning component is destroyed', () => {
   const Vue = createLocalVue();
   Vue.use(VeeValidate);
 
-  const wrapper = mount(TestComponent, { localVue: Vue });
+  const wrapper = mount(InjectComponent, { localVue: Vue });
   const childWithParentValidator = wrapper.find(ChildInject);
   const childWithNewValidator = wrapper.find(ChildNew);
 
