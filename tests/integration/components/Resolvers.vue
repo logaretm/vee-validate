d8c68fbedf00eb3a5932dcc9823a59c62608c699
Abdelrahman Awad <logaretm1@gmail.com>
2017-12-14 23:26:52 +0200

    test value resolution for native inputs and custom components

---
+++ tests/integration/components/Resolvers.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <custom-input-no-resolver v-validate="'required'" data-vv-name="no-resolver" />
+    <custom-input-with-resolver v-validate="'required'" data-vv-name="with-resolver" />
+  </div>
+</template>
+
+<script>
+import CustomInputNoResolver from './stubs/InputWithoutValueResolver';
+import CustomInputWithResolver from './stubs/Input';
+
+export default {
+  name: 'resolvers-test',
+  components: {
+    CustomInputNoResolver,
+    CustomInputWithResolver
+  }
+}
+</script>
