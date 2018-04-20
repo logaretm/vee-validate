a0b3f066e21002e5abb865243b99ebace8b6ef7c
Abdelrahman Awad <logaretm1@gmail.com>
2018-04-20 19:26:27 +0200

    vuepress config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  title: 'VeeValidate',
+  description: 'Rich input validation framework for Vue.js',
+  ga: 'UA-100131478-1',
+  serviceWorker: true,
+  base: '/vee-validate/',
+  head: [
+    ['meta', { charset: 'utf-8' }],
+    ['meta', { name: "msapplication-TileColor", content: "#ffffff" }],
+    ['meta', { name: "msapplication-TileImage", content: "/img/ms-icon-144x144.png" }],
+    ['meta', { name: "theme-color", content: "#41b883" }],
+    ['meta', { name: 'viewport', content: 'widt:device-width, initial-scal:1' }],
+    ['meta', { property: 'og:image', content: 'https://s3.eu-central-1.amazonaws.com/logaretm/vee-validate.svg' }],
+    ['link', { rel: "apple-touch-icon", sizes: "57x57", href: "/img/apple-icon-57x57.png" }],
+    ['link', { rel: "apple-touch-icon", sizes: "60x60", href: "/img/apple-icon-60x60.png" }],
+    ['link', { rel: "apple-touch-icon", sizes: "72x72", href: "/img/apple-icon-72x72.png" }],
+    ['link', { rel: "apple-touch-icon", sizes: "76x76", href: "/img/apple-icon-76x76.png" }],
+    ['link', { rel: "apple-touch-icon", sizes: "114x114", href: "/img/apple-icon-114x114.png" }],
+    ['link', { rel: "apple-touch-icon", sizes: "120x120", href: "/img/apple-icon-120x120.png" }],
+    ['link', { rel: "apple-touch-icon", sizes: "144x144", href: "/img/apple-icon-144x144.png" }],
+    ['link', { rel: "apple-touch-icon", sizes: "152x152", href: "/img/apple-icon-152x152.png" }],
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/img/apple-icon-180x180.png" }],
+    ['link', { rel: "icon", type: "image/png", sizes: "192x192", href: "/img/android-icon-192x192.png" }],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/img/favicon-32x32.png" }],
+    ['link', { rel: "icon", type: "image/png", sizes: "96x96", href: "/img/favicon-96x96.png" }],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/img/favicon-16x16.png" }]
+  ],
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'Validation', link: '/localization/' },
+      { text: 'Localization', link: '/localization/' },
+      { text: 'Examples', link: '/localization/' },
+      { text: 'API', link: '/api/' }
+    ]
+  }
+};
