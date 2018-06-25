318477901e1848ca3320fe86016d870cc1e23ac4
Abdelrahman Awad <logaretm1@gmail.com>
2018-06-25 04:51:48 +0200

    tests: coverage for mixin.js

--- tests/integration/components/Flags.vue
+++ tests/integration/components/Flags.vue
@@ -2,6 +2,9 @@
   <div>
     <input type="text" name="name" id="name" v-validate="'required'">
     <span v-if="fields.name && fields.name.invalid" id="error">{{ errors.first('name') }}</span>
+
+    <input type="text" name="scoped" id="scoped" v-validate="'required'" data-vv-scope="s1">
+    <span v-if="fields.$s1 && fields.$s1.name && fields.$s1.name.invalid" id="scopeError">{{ errors.first('scoped') }}</span>
   </div>
 </template>
 
