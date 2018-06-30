a887b126b8500d224aded99a1583f7b4e20016e6
Abdelrahman Awad <logaretm1@gmail.com>
2018-06-30 21:57:36 +0300

    tests: test the directive bails and continues modifiers

---
+++ tests/integration/components/BailContinue.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <input type="text" name="bails" v-validate.bails="'required|min:3|is:3'">
+    <input type="text" name="continues" v-validate.continues="'required|min:3|is:3'">
+    <input type="text" name="undefined" v-validate="'required|min:3|is:3'">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'basic-test'
+};
+</script>
