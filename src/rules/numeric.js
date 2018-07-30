14775f10dba3639f91197ae1ebc8d10a9610e034
h-ARTS <hi5hanan@yahoo.com>
2018-07-30 10:54:48 +0200

    useless escape character removed

--- src/rules/numeric.js
+++ src/rules/numeric.js
@@ -1,9 +1,9 @@
 const validate = (value) => {
   if (Array.isArray(value)) {
-    return value.every(val => /^\-?[0-9]+$/.test(String(val)));
+    return value.every(val => /^-?[0-9]+$/.test(String(val)));
   }
 
-  return /^\-?[0-9]+$/.test(String(value));
+  return /^-?[0-9]+$/.test(String(value));
 };
 
 export {
