e4d3e3c5467d6a143f8d6eec7ede174039119dc2
Abdelrahman Awad <logaretm1@gmail.com>
2018-09-14 00:59:50 +0200

    feat: working draft with errors and flags

--- src/components.js
+++ src/components.js
@@ -1,13 +1,39 @@
 import Config from './config';
-import { createFlags } from './core/utils';
+import Validator from './core/validator';
+import { createFlags, find, toArray, assign } from './utils';
 
 let $validator = null;
+
+function hasModel (vnode) {
+  return !!(vnode.data && vnode.data.directives && find(vnode.data.directives, d => d.name === 'model'));
+}
+
+function getModel (vnode) {
+  return find(vnode.data.directives, d => d.name === 'model');
+}
+
+function findModelNode (vnode) {
+  if (hasModel(vnode)) {
+    return vnode;
+  }
+
+  if (vnode.children && vnode.children.length) {
+    return toArray(vnode.children).reduce((candidate, node) => {
+      if (candidate) {
+        return candidate;
+      }
+
+      return findModelNode(node);
+    }, null);
+  }
+
+  return null;
+}
 
 export const ValidationProvider = {
   props: {
     name: {
-      type: String,
-      required: true,
+      type: String
     },
     alias: {
       type: String
@@ -21,23 +47,25 @@ export const ValidationProvider = {
   },
   data: () => ({
     errors: [],
-    flags: createFlags()
+    flags: createFlags(),
+    hooked: false
   }),
   computed: {
     classes () {
       const names = Config.current.classNames;
-      return Object.keys(this.flags).reduce((flag, classes) => {
-        if (names[flag]) {
-          classes[names[flag]] = this.flags[flag];
+      return Object.keys(this.flags).reduce((classes, flag) => {
+        const className = (names && names[flag]) || flag;
+        if (className) {
+          classes[className] = this.flags[flag];
         }
 
         return classes;
       }, {});
     }
   },
-  render () {
+  render (h) {
     if (!$validator) {
-      $validator = Config.dependency('validator');
+      $validator = new Validator(null);
     }
 
     const ctx = {
@@ -46,9 +74,24 @@ export const ValidationProvider = {
       classes: this.classes
     };
 
-    const vnode = this.$scopedSlots.default(ctx);
-    console.log(vnode);
+    const render = h('div', [this.$scopedSlots.default(ctx)]);
+    if (!this.hooked) {
+      const nodeWithModel = findModelNode(render);
+      const model = getModel(nodeWithModel);
+      this.$parent.$watch(model.expression, (val) => {
+        $validator.verify(val, this.rules).then(({ errors }) => {
+          this.errors = errors;
 
-    return vnode;
+          // TODO: Set ALL FLAGS
+          this.flags = assign({}, this.flags, {
+            valid: !errors.length,
+            invalid: !!errors.length
+          });
+        });
+      });
+      this.hooked = true;
+    }
+
+    return render;
   }
 };
