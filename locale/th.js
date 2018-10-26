44a71b370114360ad8448999b4fc9a9f6f73c20d
Anuruk S <idrafttt@gmail.com>
2018-10-26 19:02:52 +0700

    Update th.js (#1675)
    This PR improves the locale file th.js

--- locale/th.js
+++ locale/th.js
@@ -10,21 +10,21 @@ const messages = {
   before: (field, [target]) => `${field} ต้องเป็นวันที่ก่อน ${target}`,
   between: (field, [min, max]) => `${field} ต้องเป็นค่าระหว่าง ${min} และ ${max}`,
   confirmed: (field) => `การยืนยันข้อมูลของ ${field} ไม่ตรงกัน`,
-  credit_card: (field) => `ข้อมูล ${field} ไม่ถูกต้อง`,
+  credit_card: (field) => `หมายเลขบัตรเครดิตของ ${field} ไม่ถูกต้อง`,
   date_between: (field, [min, max]) => `วันที่ ${field} ต้องอยู่ระหว่าง ${min} และ ${max}`,
-  date_format: (field, [format]) => `วันที่ ${field} ต้องอยู่ในรูปแบบ ${format}`,
+  date_format: (field, [format]) => `วันที่ ${field} ต้องเป็นรูปแบบ ${format}`,
   decimal: (field, [decimals = '*'] = []) => `${field} ต้องเป็นตัวเลข และสามารถมีจุดทศนิยม ${decimals === '*' ? '' : decimals + ' จุด'}`,
-  digits: (field, [length]) => `${field} ต้องเป็นตัวเลข ${length} หลักเท่านั้น`,
-  dimensions: (field, [width, height]) => `${field} ต้องมีขนาด ${width} x ${height} px`,
+  digits: (field, [length]) => `${field} ต้องเป็นตัวเลขจำนวน ${length} หลักเท่านั้น`,
+  dimensions: (field, [width, height]) => `${field} ต้องมีขนาด ${width}x${height} px`,
   email: (field) => `${field} ต้องเป็นรูปแบบอีเมล`,
   ext: (field) => `${field} สกุลไฟล์ไม่ถูกต้อง`,
   image: (field) => `${field} ต้องเป็นรูปภาพเท่านั้น`,
-  included: (field) => `${field} เป็นค่าทีไม่ถูกต้องตามเงื่อนไขที่กำหนด`,
+  included: (field) => `${field} ต้องเป็นค่าที่กำหนดเท่านั้น`,
   integer: (field) => `${field} ต้องเป็นเลขจำนวนเต็ม`,
-  ip: (field) => `${field} ไม่ถูกต้องตามรูปแบบ ip address`,
+  ip: (field) => `${field} ไม่ถูกต้องตามรูปแบบ IP address`,
   length: (field, [length, max]) => {
     if (max) {
-      return `ความยาว ${field} อยู่ระหว่าง ${length} และ ${max}`;
+      return `${field} ต้องมีความยาวระหว่าง ${length} และ ${max}`;
     }
 
     return `${field} ต้องมีความยาว ${length}`;
@@ -34,11 +34,11 @@ const messages = {
   mimes: (field) => `${field} ประเภทไฟล์ไม่ถูกต้อง`,
   min: (field, [length]) => `${field} ต้องมีความยาวอย่างน้อย ${length} ตัวอักษร`,
   min_value: (field, [min]) => `${field} ต้องมีค่าตั้งแต่ ${min} ขึ้นไป`,
-  excluded: (field) => `${field} เป็นค่าทีไม่ถูกต้องตามเงื่อนไขที่กำหนด`,
+  excluded: (field) => `${field} ต้องเป็นค่าที่กำหนดเท่านั้น`,
   numeric: (field) => `${field} ต้องเป็นตัวเลขเท่านั้น`,
   regex: (field) => `รูปแบบ ${field} ไม่ถูกต้อง`,
   required: (field) => `กรุณากรอก ${field}`,
-  size: (field, [size]) => `${field} ต้องมีขนาดไม่เกิน ${formatFileSize(size)}`,
+  size: (field, [size]) => `${field} ต้องมีขนาดไฟล์ไม่เกิน ${formatFileSize(size)}`,
   url: (field) => `${field} ไม่ใช่รูปแบบของ URL ที่ถูกต้อง`
 };
 
