7efdc5ced79eefedb8b90df9e6f9d6b64a054111
Simon Tarchichi <kartsims@gmail.com>
2018-10-27 11:55:18 +0200

    types definition : 'validate' name param is optional (#1612)
    Got a small error using Typescript, the [`validate`](https://baianat.github.io/vee-validate/api/validator.html#api) method's `name` param is supposed to be optional

--- types/vee-validate.d.ts
+++ types/vee-validate.d.ts
@@ -158,7 +158,7 @@ export class Validator {
     localize(rootDictionary?: Object) :void;
     localize(language: string, dictionary?: Object) :void;
     setStrictMode(strictMode?: boolean): void;
-    validate(name: string, value?: any, scope?: string, silent?: boolean): Promise<any>;
+    validate(name?: string, value?: any, scope?: string, silent?: boolean): Promise<any>;
     validateAll(values?: Object, scope?: string, silent?: boolean): Promise<any>;
     validateScopes(silent?: boolean): Promise<any>;
     verify(value: any, rules: string|Object, options?: VerifyOptions): Promise<VerifyResult>;
