144e09132f149e78b432c7585cddb35b60b44192
Abdelrahman Awad <logaretm1@Gmail.com>
2019-02-13 00:00:51 +0200

    Cleanup (#1853)
    * style: use haveLength in persist modifier tests
    
    * chore: remove unused script
    
    * chore: remove unused scripts

--- tests/integration/persist.js
+++ tests/integration/persist.js
@@ -13,36 +13,36 @@ test('persist fields should stay in the field list, as well as their errors', as
 
   // await new Promise(resolve => wrapper.vm.$nextTick(() => resolve()));
   expect(wrapper.vm.errors.count()).toBe(2);
-  expect(Object.keys(wrapper.vm.fields).length).toBe(4);
+  expect(Object.keys(wrapper.vm.fields)).toHaveLength(4);
 
   wrapper.vm.displayFields = false; // We hide all the fields
   await flushPromises();
 
   expect(wrapper.vm.errors.count()).toBe(1); // Only one persist field is errored (because one is immediate)
-  expect(Object.keys(wrapper.vm.fields).length).toBe(2); // We expect the persist fields to be there
+  expect(Object.keys(wrapper.vm.fields)).toHaveLength(2); // We expect the persist fields to be there
 
   wrapper.vm.displayFields = true;
   await flushPromises();
 
   expect(wrapper.vm.errors.count()).toBe(2);
-  expect(Object.keys(wrapper.vm.fields).length).toBe(4); // The four fields are visible
+  expect(Object.keys(wrapper.vm.fields)).toHaveLength(4); // The four fields are visible
 
   expect(await wrapper.vm.$validator.validateAll()).toBe(false); // 4 required -> 4 errors
 
   expect(wrapper.vm.errors.count()).toBe(4); // The .validateAll call has triggered 4 errors
-  expect(Object.keys(wrapper.vm.fields).length).toBe(4);
+  expect(Object.keys(wrapper.vm.fields)).toHaveLength(4);
 
   wrapper.vm.displayFields = false;
   await flushPromises();
 
   expect(wrapper.vm.errors.count()).toBe(2); // The two persist fields are still errored
-  expect(Object.keys(wrapper.vm.fields).length).toBe(2);
+  expect(Object.keys(wrapper.vm.fields)).toHaveLength(2);
 
   wrapper.vm.displayFields = true;
   await flushPromises();
 
   expect(wrapper.vm.errors.count()).toBe(3); // The two persist fields are still errored
-  expect(Object.keys(wrapper.vm.fields).length).toBe(4);
+  expect(Object.keys(wrapper.vm.fields)).toHaveLength(4);
 
   // The `valid` flag must stay the same as before
   expect(wrapper.vm.fields['field-persist'].valid).toBe(false);
@@ -53,5 +53,5 @@ test('persist fields should stay in the field list, as well as their errors', as
   await flushPromises();
 
   expect(wrapper.vm.errors.count()).toBe(1);
-  expect(Object.keys(wrapper.vm.fields).length).toBe(1);
+  expect(Object.keys(wrapper.vm.fields)).toHaveLength(1);
 });
