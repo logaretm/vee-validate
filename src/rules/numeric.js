fadf704a60fd54d3e95c1df3a0e65466f76fcb44
Abdelrahman Awad <logaretm1@Gmail.com>
2019-03-11 16:45:10 +0200

    feat: added arabic numerics to the numeric rule (#1918)

--- src/rules/numeric.js
+++ src/rules/numeric.js
@@ -1,9 +1,19 @@
+
+const ar = /^[٠١٢٣٤٥٦٧٨٩]+$/;
+const en = /^[0-9]+$/;
+
 const validate = (value) => {
+  const testValue = val => {
+    const strValue = String(val);
+
+    return en.test(strValue) || ar.test(strValue);
+  };
+
   if (Array.isArray(value)) {
-    return value.every(val => /^[0-9]+$/.test(String(val)));
+    return value.every(testValue);
   }
 
-  return /^[0-9]+$/.test(String(value));
+  return testValue(value);
 };
 
 export {
