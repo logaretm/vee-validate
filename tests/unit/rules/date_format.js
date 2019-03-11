da4a35bd38c3c02b0ef5b21c126809e6eb2f10d6
Abdelrahman Awad <logaretm1@Gmail.com>
2019-03-11 17:49:21 +0200

    fix: update date-fns to account for the offset issues in firefox/chro… (#1698)
    🔎 __Overview__
    
    This PR updates date-fns pkg past the alpha.7 release, which introduces some breaking changes to the date-format.
    
    https://date-fns.org/v2.0.0-alpha.25/docs/parse
    
    https://github.com/date-fns/date-fns/blob/master/docs/unicodeTokens.md
    
    ✔ __Issues affected__
    
    closes #1693, closes #1904

--- tests/unit/rules/date_format.js
+++ tests/unit/rules/date_format.js
@@ -1,9 +1,9 @@
 import { validate } from '@/rules/date_format';
 
 test('validates a date format', () => {
-  expect(validate('2010-10-20 04:30', { format: 'YYYY-MM-DD HH:mm' })).toBe(true);
-  expect(validate('2010-10-20 4:30', { format: 'YYYY-MM-DD HH:mm' })).toBe(false);
-  expect(validate('2010-10-20', { format: 'YYYY-MM-DD HH:mm' })).toBe(false);
-  expect(validate('2010-10-20 24:01', { format: 'YYYY-MM-DD HH:mm' })).toBe(false);
-  expect(validate('2010-10-20 4:61', { format: 'YYYY-MM-DD HH:mm' })).toBe(false);
+  expect(validate('2010-10-20 04:30', { format: 'yyyy-MM-dd HH:mm' })).toBe(true);
+  expect(validate('2010-10-20 4:30', { format: 'yyyy-MM-dd HH:mm' })).toBe(false);
+  expect(validate('2010-10-20', { format: 'yyyy-MM-dd HH:mm' })).toBe(false);
+  expect(validate('2010-10-20 24:01', { format: 'yyyy-MM-dd HH:mm' })).toBe(false);
+  expect(validate('2010-10-20 4:61', { format: 'yyyy-MM-dd HH:mm' })).toBe(false);
 });
