922e040fd687946998f64875390cbadd9aac1344
Abdelrahman Awad <logaretm1@gmail.com>
2019-03-23 17:53:26 +0200

    test: customizing validation events

--- tests/integration/events.js
+++ tests/integration/events.js
@@ -1,48 +1,59 @@
-import { shallow, createLocalVue } from '@vue/test-utils';
+import { mount, createLocalVue } from '@vue/test-utils';
 import flushPromises from 'flush-promises';
 import VeeValidate from '@/index';
-import TestComponent from './components/Basic';
-import CustomEventTestComponent from './components/Events';
 
-test('can customize validation trigger events via global config', async () => {
-  const Vue = createLocalVue();
-  Vue.use(VeeValidate, {
-    events: 'blur|custom'
+describe('Setting custom events for validation', () => {
+  test('can customize validation trigger events via global config', async () => {
+    const Vue = createLocalVue();
+    Vue.use(VeeValidate, {
+      events: 'blur|custom'
+    });
+
+    const wrapper = mount({
+      template: `
+      <div>
+        <input type="text" name="field" v-validate="'required'">
+        <span>{{ errors.count() }}</span>
+      </div>
+    `
+    }, { localVue: Vue });
+
+    const input = wrapper.find('input');
+    const count = wrapper.find('span');
+
+    expect(count.text()).toBe('0');
+    input.trigger('input');
+    await flushPromises();
+    expect(count.text()).toBe('0');
+
+    input.trigger('blur');
+    await flushPromises();
+
+    expect(count.text()).toBe('1');
   });
 
-  const wrapper = shallow(TestComponent, { localVue: Vue });
+  test('can customize events per field via data-vv-validate-on attribute', async () => {
+    const Vue = createLocalVue();
+    Vue.use(VeeValidate);
+    const wrapper = mount({
+      template: `
+      <div>
+        <input type="text" name="field" v-validate="'required'" data-vv-as="custom">
+        <span>{{ errors.count() }}</span>
+      </div>
+    `
+    }, { localVue: Vue });
 
-  const input = wrapper.find('input');
-  expect(wrapper.vm.errors.count()).toBe(0);
-  input.trigger('input');
-  await flushPromises();
-  expect(wrapper.vm.errors.count()).toBe(0); // no validation triggered
+    const input = wrapper.find('input');
+    const count = wrapper.find('span');
 
-  input.trigger('blur');
-  await flushPromises();
+    expect(count.text()).toBe('0');
+    input.trigger('input');
+    await flushPromises();
+    expect(count.text()).toBe('0');
 
-  expect(wrapper.vm.errors.count()).toBe(1);
-  wrapper.vm.errors.clear();
-  expect(wrapper.vm.errors.count()).toBe(0);
-
-  input.trigger('custom');
-  await flushPromises();
-  expect(wrapper.vm.errors.count()).toBe(1);
-});
-
-test('can customize events per field via data-vv-validate-on attribute', async () => {
-  const Vue = createLocalVue();
-  Vue.use(VeeValidate);
-
-  const wrapper = shallow(CustomEventTestComponent, { localVue: Vue });
-
-  const input = wrapper.find('input');
-  expect(wrapper.vm.errors.count()).toBe(0);
-  input.trigger('input');
-  await flushPromises();
-  expect(wrapper.vm.errors.count()).toBe(0); // no validation triggered
-
-  input.trigger('custom');
-  await flushPromises();
-  expect(wrapper.vm.errors.count()).toBe(1);
+    input.trigger('custom');
+    await flushPromises();
+    expect(count.text()).toBe('1');
+  });
 });
