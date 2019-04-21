fa59a29952e7d3afbf93e26258933078d2156452
Abdelrahman Awad <logaretm1@gmail.com>
2019-04-21 12:06:47 +0200

    chore: added .prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  trailingComma: 'none',
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  endOfLine: 'lf'
+};
