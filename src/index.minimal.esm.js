c2838d63ebb820371efb2293f22261ee73336451
Ctibor Laky <luckylooke@gmail.com>
2019-04-26 14:18:55 +0200

    feat: setMode in minimal bundle size usage method of the lib (#2044) (#2045)
    Closes #2044

--- src/index.minimal.esm.js
+++ src/index.minimal.esm.js
@@ -9,10 +9,12 @@ import { ValidationProvider, ValidationObserver, withValidation } from './compon
 const version = '__VERSION__';
 const install = VeeValidate.install;
 const use = VeeValidate.use;
+const setMode = VeeValidate.setMode;
 
 export {
   install,
   use,
+  setMode,
   directive,
   mixin,
   mapFields,
