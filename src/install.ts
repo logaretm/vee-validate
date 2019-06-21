d976165939eb77b0a2fee74d7c6e1e8e6e55475b
Abdelrahman Awad <logaretm1@gmail.com>
2019-06-21 16:48:37 +0200

    chore: remove unused imports in the install fn

--- src/install.ts
+++ src/install.ts
@@ -1,9 +1,5 @@
 import { VeeValidateConfig, setConfig } from './config';
-import * as Rules from './rules';
-import en from '../locale/en';
-import { extend } from './extend';
-import { Dictionary, RootI18nDictionary } from './dictionary';
 
-export function install(Vue: any, config: VeeValidateConfig) {
+export function install(_: any, config: VeeValidateConfig) {
   setConfig(config);
 }
