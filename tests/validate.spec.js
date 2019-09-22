e54377d272e74dc1e29c58a8d5e94f07ed1d93fc
Dave Stewart <dave@davestewart.co.uk>
2019-09-22 18:43:22 +0100

    test: add tests for `_target_` placeholder population

--- tests/validate.spec.js
+++ tests/validate.spec.js
@@ -1,5 +1,6 @@
 import { validate } from '@/validate';
 import { extend } from '@/extend';
+import { confirmed } from '@/rules';
 
 test('returns custom error messages passed in ValidationOptions', async () => {
   extend('truthy', {
@@ -19,4 +20,36 @@ test('returns custom error messages passed in ValidationOptions', async () => {
   const result = await validate(value, rules, options);
 
   expect(result.errors[0]).toEqual(customMessage);
+});
+
+describe('target field placeholder', () => {
+  extend('confirmed', {
+    ...confirmed,
+    message: '{_field_} must match {_target_}'
+  });
+
+  const names = { foo: 'Foo', bar: 'Bar', baz: 'Baz' };
+
+  test('uses target field name, if supplied in options', async () => {
+    const values = { foo: 10, bar: 20 };
+    const rules = 'confirmed:foo';
+    const options = {
+      name: names.bar,
+      values,
+      names
+    };
+    const result = await validate(values.bar, rules, options);
+    expect(result.errors[0]).toEqual('Bar must match Foo');
+  });
+
+  test('uses target field key, if target field name not supplied in options', async () => {
+    const values = { foo: 10, bar: 20 };
+    const rules = 'confirmed:foo';
+    const options = {
+      name: names.bar,
+      values
+    };
+    const result = await validate(values.bar, rules, options);
+    expect(result.errors[0]).toEqual('Bar must match foo');
+  });
 });
