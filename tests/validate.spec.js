8c21df3311ad5726002e8a2f8cdcfadbbaf64ea0
Dave Stewart <dave@davestewart.co.uk>
2019-09-22 20:39:14 +0100

    test: add tests for multiple target placeholders in error messages

--- tests/validate.spec.js
+++ tests/validate.spec.js
@@ -52,4 +52,28 @@ describe('target field placeholder', () => {
     const result = await validate(values.bar, rules, options);
     expect(result.errors[0]).toEqual('Bar must match foo');
   });
+
+  test('works for multiple targets', async () => {
+    extend('sum_of', {
+      message: '{_field_} must be the sum of {_aTarget_} and {_bTarget_}',
+      // eslint-disable-next-line prettier/prettier
+      params: [
+        { name: 'a', isTarget: true },
+        { name: 'b', isTarget: true }
+      ],
+      validate: (value, { a, b }) => value === parseInt(a, 10) + parseInt(b, 10)
+    });
+
+    const values = { foo: 10, bar: 10, baz: 10 };
+    const names = { foo: 'Foo', bar: 'Bar', baz: 'Baz' };
+    const rules = 'sum_of:bar,baz';
+    const options = {
+      name: names.foo,
+      values,
+      names
+    };
+
+    const result = await validate(values.foo, rules, options);
+    expect(result.errors[0]).toEqual('Foo must be the sum of Bar and Baz');
+  });
 });
