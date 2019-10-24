a8fe5297e27888cb9ad7f73dd9d2bf7b6b0df1e8
Dave Stewart <dave@davestewart.co.uk>
2019-10-24 09:09:00 +0100

    style: remove redundant escapes from regex

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -9,7 +9,8 @@ export function warn(message: string) {
  * Replaces placeholder values in a string with their actual values
  */
 export function interpolate(template: string, values: Record<string, any>): string {
-  return template.replace(/\{([^}]+)\}/g, (_, p): string => {
+  const rx = /{([^}]+)}/g;
+  return template.replace(rx, (_, p): string => {
     return p in values ? values[p] : `{${p}}`;
   });
 }
