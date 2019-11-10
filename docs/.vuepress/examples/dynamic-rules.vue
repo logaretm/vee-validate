2c54542880aa29f87d9be1d8b90c4773bf4d3c01
Abdelrahman Awad <logaretm1@gmail.com>
2019-11-10 20:06:39 +0200

    docs: added dynamic rules section

---
+++ docs/.vuepress/examples/dynamic-rules.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ValidationProvider :rules="{ required }" v-slot="{ errors }">
+      <input v-model="value" type="text">
+      <span>{{ errors[0] }}</span>
+    </ValidationProvider>
+    <div>
+      <input type="checkbox" v-model="required"> Is Required?
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    value: '',
+    required: false
+  })
+};
+</script>
+
