6149b58a657a4ed6eade7f5ea62669b3ebc4a5e1
Abdelrahman Awad <logaretm1@gmail.com>
2019-11-20 22:45:15 +0200

    docs: added refactoring guide

---
+++ docs/.vuepress/examples/large-form-refactored.vue
@@ -0,0 +1,42 @@
+<template>
+  <ValidationObserver v-slot="{ handleSubmit }">
+    <form @submit.prevent="handleSubmit(onSubmit)">
+      <TextInput v-model="fname" name="First Name" rules="required" />
+
+      <TextInput v-model="lname" name="Last Name" rules="required" />
+
+      <TextInput v-model="email" name="E-mail" rules="required|email" />
+
+      <TextInput v-model="password" type="password" name="Password" rules="required|min:6|confirmed:confirmation" />
+
+      <TextInput v-model="passwordConfirmation" type="password" name="Confirmation" vid="confirmation" rules="required" />
+
+      <TextInput v-model="country" name="Country" rules="required" />
+
+      <button>Submit</button>
+    </form>
+  </ValidationObserver>
+</template>
+
+<script>
+import TextInput from './text-input';
+
+export default {
+  components: {
+    TextInput
+  },
+  data: () => ({
+    fname: '',
+    lname: '',
+    email: '',
+    password: '',
+    passwordConfirmation: '',
+    country: ''
+  }),
+  methods: {
+    onSubmit () {
+      alert('Form submitted!');
+    }
+  }
+}
+</script>
---
+++ docs/.vuepress/examples/large-form-unrefactored.vue
@@ -0,0 +1,55 @@
+<template>
+  <ValidationObserver v-slot="{ handleSubmit }">
+    <form @submit.prevent="handleSubmit(onSubmit)">
+      <ValidationProvider tag="div" rules="required" name="First Name" v-slot="{ errors }">
+        <input v-model="fname" type="text">
+        <span>{{ errors[0] }}</span>
+      </ValidationProvider>
+
+      <ValidationProvider tag="div" rules="required" name="Last Name" v-slot="{ errors }">
+        <input v-model="lname" type="text">
+        <span>{{ errors[0] }}</span>
+      </ValidationProvider>
+
+      <ValidationProvider tag="div" rules="required|email" name="E-mail" v-slot="{ errors }">
+        <input v-model="email" type="text">
+        <span>{{ errors[0] }}</span>
+      </ValidationProvider>
+
+      <ValidationProvider tag="div" rules="required|min:6|confirmed:confirmation" name="password" v-slot="{ errors }">
+        <input v-model="password" type="password">
+        <span>{{ errors[0] }}</span>
+      </ValidationProvider>
+
+      <ValidationProvider tag="div" vid="confirmation" rules="required" name="Confirmation" v-slot="{ errors }">
+        <input v-model="passwordConfirmation" type="password">
+        <span>{{ errors[0] }}</span>
+      </ValidationProvider>
+
+      <ValidationProvider tag="div" name="Country" rules="required" v-slot="{ errors }">
+        <input v-model="country" type="text">
+        <span>{{ errors[0] }}</span>
+      </ValidationProvider>
+
+      <button>Submit</button>
+    </form>
+  </ValidationObserver>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    fname: '',
+    lname: '',
+    email: '',
+    password: '',
+    passwordConfirmation: '',
+    country: ''
+  }),
+  methods: {
+    onSubmit () {
+      alert('Form submitted!');
+    }
+  }
+}
+</script>
