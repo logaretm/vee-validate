b2f1bbf02faafe967eb57240ccf6ce77841d67cd
Abdelrahman Awad <logaretm1@gmail.com>
2019-11-25 19:53:15 +0200

    fix: why the fk does this even work closes #2508

--- src/components/Observer.ts
+++ src/components/Observer.ts
@@ -1,4 +1,4 @@
-import Vue, { CreateElement, VNode, VueConstructor } from 'vue';
+import Vue, { VueConstructor } from 'vue';
 import { values, findIndex, debounce } from '../utils';
 import { ValidationResult, VeeObserver, VNodeWithVeeContext, ValidationFlags, KnownKeys } from '../types';
 import { ValidationProvider } from './Provider';
@@ -23,6 +23,13 @@ type ObserverErrors = Record<string, string[]>;
 
 let OBSERVER_COUNTER = 0;
 
+type withObserverNode = VueConstructor<
+  Vue & {
+    $_veeObserver: VeeObserver;
+    $vnode: VNodeWithVeeContext;
+  }
+>;
+
 function data() {
   const refs: Record<string, ProviderInstance> = {};
   const errors: ObserverErrors = {};
@@ -39,12 +46,65 @@ function data() {
   };
 }
 
-type withObserverNode = VueConstructor<
-  Vue & {
-    $_veeObserver: VeeObserver;
-    $vnode: VNodeWithVeeContext;
+const methods = {
+  subscribe(this: any, subscriber: any, kind = 'provider') {
+    if (kind === 'observer') {
+      this.observers.push(subscriber);
+      return;
+    }
+
+    this.refs = { ...this.refs, ...{ [subscriber.id]: subscriber } };
+  },
+  unsubscribe(this: any, id: string, kind = 'provider') {
+    if (kind === 'provider') {
+      const provider = this.refs[id];
+      if (!provider) {
+        return;
+      }
+
+      this.$delete(this.refs, id);
+      return;
+    }
+
+    const idx = findIndex(this.observers, (o: any) => o.id === id);
+    if (idx !== -1) {
+      this.observers.splice(idx, 1);
+    }
+  },
+  async validate(this: any, { silent = false }: { silent?: boolean } = {}) {
+    const results = await Promise.all([
+      ...values(this.refs)
+        .filter((r: any) => !r.disabled)
+        .map((ref: any) => ref[silent ? 'validateSilent' : 'validate']().then((r: ValidationResult) => r.valid)),
+      ...this.observers.filter((o: any) => !o.disabled).map((obs: any) => obs.validate({ silent }))
+    ]);
+
+    return results.every(r => r);
+  },
+  async handleSubmit(cb: Function) {
+    const isValid = await this.validate();
+    if (!isValid || !cb) {
+      return;
+    }
+
+    return cb();
+  },
+  reset(this: any) {
+    return [...values(this.refs), ...this.observers].forEach(ref => ref.reset());
+  },
+  setErrors(this: any, errors: Record<string, string[]>) {
+    Object.keys(errors).forEach(key => {
+      const provider = this.refs[key];
+      if (!provider) return;
+
+      provider.setErrors(errors[key] || []);
+    });
+
+    this.observers.forEach((observer: any) => {
+      observer.setErrors(errors);
+    });
   }
->;
+};
 
 export const ValidationObserver = (Vue as withObserverNode).extend({
   name: 'ValidationObserver',
@@ -88,119 +148,63 @@ export const ValidationObserver = (Vue as withObserverNode).extend({
   data,
   created() {
     this.id = this.vid;
-    register.call(this);
+    register(this);
 
     const onChange = debounce(({ errors, flags }: { errors: ObserverErrors; flags: ValidationFlags }) => {
       this.errors = errors;
       this.flags = flags;
     }, 16);
 
-    this.$watch(
-      () => {
-        const vms = [...values(this.refs), ...this.observers];
-        const errors: ObserverErrors = {};
-        const flags: ValidationFlags = {} as ValidationFlags;
+    this.$watch(() => {
+      const vms = [...values(this.refs), ...this.observers];
+      const errors: ObserverErrors = {};
+      const flags: ValidationFlags = {} as ValidationFlags;
 
-        const length = vms.length;
-        for (let i = 0; i < length; i++) {
-          const vm = vms[i];
-          errors[vm.id] = vm.errors;
-        }
+      const length = vms.length;
+      for (let i = 0; i < length; i++) {
+        const vm = vms[i];
+        errors[vm.id] = vm.errors;
+      }
 
-        FLAGS_STRATEGIES.forEach(([flag, method]) => {
-          flags[flag] = vms[method](vm => vm.flags[flag]);
-        });
+      FLAGS_STRATEGIES.forEach(([flag, method]) => {
+        flags[flag] = vms[method](vm => vm.flags[flag]);
+      });
 
-        return { errors, flags };
-      },
-      onChange as any
-    );
+      return { errors, flags };
+    }, onChange as any);
   },
-  activated: register,
-  deactivated: unregister,
-  beforeDestroy: unregister,
-  render(h: CreateElement): VNode {
+  activated() {
+    register(this);
+  },
+  deactivated() {
+    unregister(this);
+  },
+  beforeDestroy() {
+    unregister(this);
+  },
+  render(h) {
     const children = normalizeChildren(this, prepareSlotProps(this));
 
     return this.slim && children.length <= 1 ? children[0] : h(this.tag, { on: this.$listeners }, children);
   },
-  methods: {
-    subscribe(this: any, subscriber: any, kind = 'provider') {
-      if (kind === 'observer') {
-        this.observers.push(subscriber);
-        return;
-      }
-
-      this.refs = { ...this.refs, ...{ [subscriber.id]: subscriber } };
-    },
-    unsubscribe(this: any, id: string, kind = 'provider') {
-      if (kind === 'provider') {
-        this.removeProvider(id);
-        return;
-      }
-
-      const idx = findIndex(this.observers, (o: any) => o.id === id);
-      if (idx !== -1) {
-        this.observers.splice(idx, 1);
-      }
-    },
-    async validate({ silent = false }: { silent?: boolean } = {}) {
-      const results = await Promise.all([
-        ...values(this.refs)
-          .filter(r => !r.disabled)
-          .map((ref: any) => ref[silent ? 'validateSilent' : 'validate']().then((r: ValidationResult) => r.valid)),
-        ...this.observers.filter(o => !o.disabled).map((obs: any) => obs.validate({ silent }))
-      ]);
-
-      return results.every(r => r);
-    },
-    async handleSubmit(this: any, cb: Function) {
-      const isValid = await this.validate();
-      if (!isValid || !cb) {
-        return;
-      }
-
-      return cb();
-    },
-    reset() {
-      return [...values(this.refs), ...this.observers].forEach(ref => ref.reset());
-    },
-    removeProvider(id: string) {
-      const provider = this.refs[id];
-      if (!provider) {
-        return;
-      }
-
-      this.$delete(this.refs, id);
-    },
-    setErrors(errors: Record<string, string[]>) {
-      Object.keys(errors).forEach(key => {
-        const provider = this.refs[key];
-        if (!provider) return;
-
-        provider.setErrors(errors[key] || []);
-      });
-
-      this.observers.forEach((observer: any) => {
-        observer.setErrors(errors);
-      });
-    }
-  }
+  methods
 });
 
-function unregister(this: any) {
-  if (this.$_veeObserver) {
-    this.$_veeObserver.unsubscribe(this.id, 'observer');
+type ObserverInstance = InstanceType<typeof ValidationObserver>;
+
+function unregister(vm: ObserverInstance) {
+  if (vm.$_veeObserver) {
+    vm.$_veeObserver.unsubscribe(vm.id, 'observer');
   }
 }
 
-function register(this: any) {
-  if (this.$_veeObserver) {
-    this.$_veeObserver.subscribe(this, 'observer');
+function register(vm: ObserverInstance) {
+  if (vm.$_veeObserver) {
+    vm.$_veeObserver.subscribe(vm, 'observer');
   }
 }
 
-function prepareSlotProps(vm: any) {
+function prepareSlotProps(vm: ObserverInstance) {
   return {
     ...vm.flags,
     errors: vm.errors,
