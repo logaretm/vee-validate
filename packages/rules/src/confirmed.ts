0a7dd9cdf2cf197d84ce429c470e7b3e6e3468bd
Abdelrahman Awad <logaretm1@gmail.com>
2020-01-25 01:03:02 +0200

    feat: remove deprecated isTarget option

--- packages/rules/src/confirmed.ts
+++ packages/rules/src/confirmed.ts
@@ -2,12 +2,7 @@ import { RuleParamSchema } from '@vee-validate/shared';
 
 const validate = (value: any, { target }: Record<string, any>) => String(value) === String(target);
 
-const params: RuleParamSchema[] = [
-  {
-    name: 'target',
-    isTarget: true
-  }
-];
+const params: RuleParamSchema[] = ['target'];
 
 export { validate, params };
 
