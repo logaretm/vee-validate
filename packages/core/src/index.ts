447839d93275fba7768a5dc2c82a099a827e913c
Abdelrahman Awad <logaretm1@gmail.com>
2020-01-25 22:38:36 +0200

    chore: disable component exports for now and

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,9 +1,9 @@
 export { validate } from './validate';
 export { extend } from './extend';
-export { configure } from './config';
-export { setInteractionMode } from './modes';
-export { localize } from './localize';
-export { ValidationProvider, ValidationObserver, withValidation } from './components';
+// export { configure } from './config';
+// export { setInteractionMode } from './modes';
+// export { localize } from './localize';
+// export { ValidationProvider, ValidationObserver, withValidation } from './components';
 export { normalizeRules } from './utils/rules';
 export { useField } from './useField';
 export { useForm } from './useForm';
