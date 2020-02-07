0ccdfe74fa2fbf1b61314db49c1c08c1470dc515
Abdelrahman Awad <logaretm1@gmail.com>
2020-02-07 02:47:09 +0200

    feat: override theme and add upgrade notice

---
+++ docs/.vuepress/theme/components/UpgradeNotice.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="UpgradeNotice">
+    <a href="https://logaretm.github.io/vee-validate/">VeeValidate 3 has been released 🚀</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UpgradeNotice"
+};
+</script>
+
+<style lang="stylus" scoped>
+.UpgradeNotice
+  position: fixed
+  background: #087ff4
+  width: 100%
+  height: 50px
+  display: flex
+  justify-content: center
+  align-items: center
+  z-index: 100
+
+  a
+    color: #fff
+    font-size: 18px
+    font-weight: 600
+</style>
