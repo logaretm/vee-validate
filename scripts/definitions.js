20bf7456db8c1a7fe0095205f83f5c3750d5e6c1
Eugene Strukov <eugene.strukov@gmail.com>
2020-02-28 16:19:26 +0200

    fix: make difinitions crossplatform (.sh to .js) (#2654)
    * fix: make difinitions crossplatform (.sh to .js)
    
    * fix: move shelljs to devdependencies
    
    * refactor: rewrite definitions.js with fs-extra
    
    * fix: eslint script adjusting
    
    Co-authored-by: estrukov <eugene.strukov@creuna.com>

---
+++ scripts/definitions.js
@@ -0,0 +1,41 @@
+﻿const fs = require('fs-extra');
+const path = require('path');
+const { paths } = require('./config');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+const pathTypes = path.join(paths.dist, 'types');
+const pathTypesSrc = path.join(paths.dist, 'types/src');
+
+const pathRulesSrc = path.join(paths.dist, 'types/rules/index.d.ts');
+const pathRulesDist = path.join(paths.dist, 'rules.d.ts');
+
+async function definitions() {
+  try {
+    await exec('tsc -v');
+
+    console.log('Removing old declarations...');
+    await fs.emptyDir(pathTypes);
+    console.log('Done\n');
+
+    console.log('Generating main declarations...');
+    await exec('tsc --emitDeclarationOnly');
+    console.log('Done\n');
+
+    console.log('Cleaning up declaration folder...');
+    await fs.copy(pathTypesSrc, pathTypes);
+    await fs.remove(pathTypesSrc);
+    console.log('Done\n');
+    console.log('Copying rule declarations...');
+    await fs.copy(pathRulesSrc, pathRulesDist);
+    console.log('Done\n');
+
+    console.log('Replacing definitions...');
+    await exec('node ./scripts/replaceDefs.js');
+    console.log('Done\n');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+definitions();
