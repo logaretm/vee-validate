0f7797559ca0b9ddfaeb120ec594d6037320acdd
Abdelrahman Awad <logaretm1@gmail.com>
2020-06-04 03:28:31 +0200

    test: updated required test to match new behavior

--- packages/rules/tests/required.spec.ts
+++ packages/rules/tests/required.spec.ts
@@ -1,17 +1,12 @@
 import validate from '../src/required';
 
-const valid = ['asjdj', 0, 'undefined', 'null', 's ', true, false];
+const valid = ['asjdj', 0, 'undefined', 'null', 's ', true];
 
-const invalid = ['', ' ', [], undefined, null];
+const invalid = ['', ' ', [], undefined, null, false];
 
 test('validates required', () => {
   expect.assertions(12);
   valid.forEach(value => expect(validate(value)).toBe(true));
 
   invalid.forEach(value => expect(validate(value)).toBe(false));
-});
-
-test('false value can be invalidated', () => {
-  expect(validate(false)).toBe(true);
-  expect(validate(false, { allowFalse: false })).toBe(false);
 });
--- packages/shared/src/types.ts
+++ packages/shared/src/types.ts
@@ -1,4 +1,4 @@
 export type ValidationRuleFunction = (
   value: any,
-  params: any[] | Record<string, any>
+  params?: any[] | Record<string, any>
 ) => boolean | string | Promise<boolean | string>;
