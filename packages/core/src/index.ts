b324dda3ba28fcf633fcd929232c35ca61923a5e
Abdelrahman Awad <logaretm1@gmail.com>
2020-06-20 03:25:09 +0200

    chore: remove normalizeRules export

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,6 +3,5 @@ export { defineRule } from './defineRule';
 export { configure } from './config';
 export { Field } from './Field';
 export { Form } from './Form';
-export { normalizeRules } from './utils/rules';
 export { useField } from './useField';
 export { useForm } from './useForm';
