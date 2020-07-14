373879c735cf1b0cb61061cf46a6ca87e92c8047
Abdelrahman Awad <logaretm1@gmail.com>
2020-07-14 04:43:00 +0200

    chore: added docs deploy script

---
+++ scripts/deploy-docs.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+cd packages/docs
+
+yarn build
+yarn export
+
+cd dist
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:logaretm/vee-validate.git master:gh-pages-next
+
+cd -
