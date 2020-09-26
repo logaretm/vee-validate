a19a698baa2f803e18a3f28023b5dc7697d0accb
Abdelrahman Awad <logaretm1@gmail.com>
2020-09-26 02:41:16 +0200

    docs: added custom input example

---
+++ packages/docs/components/global/CodeSandbox.vue
@@ -0,0 +1,26 @@
+<template>
+  <iframe
+    :src="src"
+    style="width: 100%; height: 500px; border: 0; border-radius: 4px; overflow: hidden; margin-top: 40px"
+    title="Custom Text Input with vee-validate v4"
+    allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+    sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+  ></iframe>
+</template>
+
+<script>
+export default {
+  name: 'CodeSandbox',
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    src() {
+      return `https://codesandbox.io/embed/${this.id}?fontsize=14&hidenavigation=1&theme=${this.$store.state.theme}`;
+    },
+  },
+};
+</script>
