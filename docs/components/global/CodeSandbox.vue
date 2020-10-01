b6e2f8412d2271e852b7000fc8fa9c37d5c64d19
Abdelrahman Awad <logaretm1@gmail.com>
2020-10-02 01:25:27 +0200

    docs: enhance sandbox component with title and view modes

--- docs/components/global/CodeSandbox.vue
+++ docs/components/global/CodeSandbox.vue
@@ -2,7 +2,7 @@
   <iframe
     :src="src"
     style="width: 100%; height: 500px; border: 0; border-radius: 4px; overflow: hidden; margin-top: 40px"
-    title="Custom Text Input with vee-validate v4"
+    :title="title"
     allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
     sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
   ></iframe>
@@ -16,10 +16,20 @@ export default {
       type: String,
       required: true,
     },
+    title: {
+      type: String,
+      required: true,
+    },
+    view: {
+      type: String,
+      default: undefined,
+    },
   },
   computed: {
     src() {
-      return `https://codesandbox.io/embed/${this.id}?fontsize=14&hidenavigation=1&theme=${this.$store.state.theme}`;
+      return `https://codesandbox.io/embed/${this.id}?fontsize=14&hidenavigation=1&theme=${this.$store.state.theme}${
+        this.view ? '&view=' + this.view : ''
+      }`;
     },
   },
 };
