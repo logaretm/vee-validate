f3785a14072b085073f9b8a13912aad202263e4c
Abdelrahman Awad <logaretm1@gmail.com>
2020-10-01 21:20:46 +0200

    feat: added validation triggers config

--- packages/core/src/config.ts
+++ packages/core/src/config.ts
@@ -3,21 +3,27 @@ import { ValidationMessageGenerator } from '../../shared';
 export interface VeeValidateConfig {
   bails: boolean;
   generateMessage: ValidationMessageGenerator;
+  validateOnInput: boolean;
+  validateOnChange: boolean;
+  validateOnBlur: boolean;
+  validateOnModelUpdate: boolean;
 }
 
 const DEFAULT_CONFIG: VeeValidateConfig = {
   generateMessage: ({ field }) => `${field} is not valid.`,
   bails: true,
+  validateOnBlur: true,
+  validateOnChange: true,
+  validateOnInput: false,
+  validateOnModelUpdate: true,
 };
 
 export let currentConfig: VeeValidateConfig = { ...DEFAULT_CONFIG };
 
 export const getConfig = () => currentConfig;
 
-export const setConfig = (newConf: Partial<VeeValidateConfig>) => {
+const setConfig = (newConf: Partial<VeeValidateConfig>) => {
   currentConfig = { ...currentConfig, ...newConf };
 };
 
-export const configure = (cfg: Partial<VeeValidateConfig>) => {
-  setConfig(cfg);
-};
+export const configure = setConfig;
