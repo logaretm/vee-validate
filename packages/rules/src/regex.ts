c29292fc6ee68580ec5fe48ac219c39d3a8b026f
Schewns <joaoazevedo.pt@gmail.com>
2020-12-01 23:16:30 +0100

    wrong rule applied? (#3056)
    shouldn't new RegExp() receive the regex formula and not the input value?

--- packages/rules/src/regex.ts
+++ packages/rules/src/regex.ts
@@ -3,7 +3,7 @@ import { getSingleParam } from './utils';
 const regexValidator = (value: any, params?: any[] | Record<string, any>): boolean => {
   let regex = getSingleParam(params, 'regex');
   if (typeof regex === 'string') {
-    regex = new RegExp(value);
+    regex = new RegExp(regex);
   }
 
   if (Array.isArray(value)) {
