51482f5e62cf80b34028292b70977f7a17e9ff71
Abdelrahman Awad <logaretm1@gmail.com>
2021-01-11 00:59:36 +0200

    docs: lazy loading for sandboxes

--- docs/components/global/CodeSandbox.vue
+++ docs/components/global/CodeSandbox.vue
@@ -1,10 +1,11 @@
 <template>
   <iframe
     :src="src"
-    style="width: 100%; height: 500px; border: 0; border-radius: 4px; overflow: hidden; margin-top: 40px"
+    style="width: 100%; height: 500px; border: 0; border-radius: 4px; overflow: hidden"
     :title="title"
     allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
     sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+    loading="lazy"
   ></iframe>
 </template>
 
