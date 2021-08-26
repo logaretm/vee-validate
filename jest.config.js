1c4bda252a1e5b050b5a1fbf78de269fd8f09b5a
Abdelrahman Awad <logaretm1@gmail.com>
2021-08-26 02:12:29 +0200

    chore: ignore devtools from coverage

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   rootDir: __dirname,
   testMatch: ['<rootDir>/packages/**/tests/**/*spec.[jt]s?(x)'],
   testPathIgnorePatterns: ['/helpers/', '/setup.ts'],
-  collectCoverageFrom: ['packages/*/src/**/*.ts'],
+  collectCoverageFrom: ['packages/*/src/**/*.ts', '!**/*/devtools.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
   moduleNameMapper: {
     '^@/(.+)$': '<rootDir>/packages/$1/src',
