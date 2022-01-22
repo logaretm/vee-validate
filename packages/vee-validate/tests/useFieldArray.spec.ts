8ca61e3aecc956759e097fd2d16420265cabd3df
Abdelrahman Awad <logaretm1@gmail.com>
2022-01-22 17:51:24 +0200

    test: added edge case test

--- packages/vee-validate/tests/useFieldArray.spec.ts
+++ packages/vee-validate/tests/useFieldArray.spec.ts
@@ -2,6 +2,34 @@ import { useForm, useFieldArray } from '@/vee-validate';
 import { onMounted } from 'vue';
 import { mountWithHoc, flushPromises } from './helpers';
 
+test('can update a field entry model directly', async () => {
+  mountWithHoc({
+    setup() {
+      useForm({
+        initialValues: {
+          users: ['1'],
+        },
+      });
+
+      const { fields } = useFieldArray('users');
+      onMounted(() => {
+        const item = fields.value[0];
+        item.value = 'test';
+      });
+
+      return {
+        fields,
+      };
+    },
+    template: `
+      <p>{{ fields[0].value }}</p>
+    `,
+  });
+
+  await flushPromises();
+  expect(document.querySelector('p')?.innerHTML).toBe('test');
+});
+
 test('warns when updating a no-longer existing item', async () => {
   const spy = jest.spyOn(console, 'warn').mockImplementation();
   mountWithHoc({
