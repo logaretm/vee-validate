13fc8be0d4dd5d003aded34743b3fad4bfa8d0a5
Abdelrahman Awad <logaretm1@gmail.com>
2022-08-07 03:07:50 +0200

    Revert "chore: added headers to netlify docs"
    This reverts commit f48d75ca847d3b107392989f0a1819cb2c1a5e4f.

--- docs/scripts/afterBuild.js
+++ docs/scripts/afterBuild.js
@@ -4,4 +4,3 @@ const path = require('path');
 const fs = require('fs');
 
 fs.copyFileSync(path.join(__dirname, '../_redirects'), path.join(__dirname, '../dist/_redirects'));
-fs.copyFileSync(path.join(__dirname, '../_headers'), path.join(__dirname, '../dist/_headers'));
