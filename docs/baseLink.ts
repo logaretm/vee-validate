d1cc641ba5df93a30e344a3ada02ca20005e4ec4
Abdelrahman Awad <logaretm1@gmail.com>
2022-08-21 16:53:27 +0200

    docs: add blank target to external links

--- docs/baseLink.ts
+++ docs/baseLink.ts
@@ -7,9 +7,31 @@ export default function addBasePath(basePath: string) {
         if (node.url.startsWith('/') && !node.url.startsWith(`${basePath}/`)) {
           node.url = `${basePath}${node.url}`;
         }
+
+        if (node.url.startsWith('http')) {
+          addProperties(node, {
+            rel: 'noopener',
+            target: '_blank',
+          });
+        }
       });
 
       return ast;
     };
+  };
+}
+
+function addProperties(node: any, props: Record<string, string>) {
+  if (!node.data) {
+    node.data = {};
+  }
+
+  if (!node.data.hProperties) {
+    node.data.hProperties = {};
+  }
+
+  node.data.hProperties = {
+    ...node.data.hProperties,
+    ...props,
   };
 }
