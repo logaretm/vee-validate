a820fba404b53f073f1bac404056a3642f313c00
Abdelrahman Awad <logaretm1@gmail.com>
2022-08-31 21:04:04 +0200

    docs: remove timers

--- docs/.vuepress/theme/components/Ad.vue
+++ docs/.vuepress/theme/components/Ad.vue
@@ -25,27 +25,9 @@ export default {
       const script = document.createElement('script');
       script.src = '//cdn.carbonads.com/carbon.js?serve=CE7DKKQ7&placement=logaretmgithubio';
       script.id = '_carbonads_js';
-      script.onload = () => {
-        setTimeout(() => {
-          this.checkIfAdLoaded();
-        }, 4000);
-      };
+
       this.$el.appendChild(script);
     },
-    checkIfAdLoaded() {
-      const adEl = document.querySelector('#carbonads');
-      if (adEl) {
-        return;
-      }
-
-      if (window._carbonads) {
-        window._carbonads.init();
-      }
-
-      setTimeout(() => {
-        this.checkIfAdLoaded();
-      }, 4000);
-    }
   }
 };
 </script>
