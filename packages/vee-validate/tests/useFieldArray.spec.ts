267736f43ca207a8fe35af30020fc61fdc009265
Abdelrahman Awad <logaretm1@Gmail.com>
2022-10-23 03:33:16 +0200

    fix: correctly mutate deep field array item and trigger validation (#3974)

--- packages/vee-validate/tests/useFieldArray.spec.ts
+++ packages/vee-validate/tests/useFieldArray.spec.ts
@@ -1,5 +1,6 @@
-import { useForm, useFieldArray } from '@/vee-validate';
-import { onMounted } from 'vue';
+import { useForm, useFieldArray, FieldEntry } from '@/vee-validate';
+import { onMounted, Ref } from 'vue';
+import * as yup from 'yup';
 import { mountWithHoc, flushPromises } from './helpers';
 
 test('can update a field entry model directly', async () => {
@@ -28,6 +29,49 @@ test('can update a field entry model directly', async () => {
 
   await flushPromises();
   expect(document.querySelector('p')?.innerHTML).toBe('test');
+});
+
+test('can update a field entry deep model directly and validate it', async () => {
+  let fields!: Ref<FieldEntry<{ name: string }>[]>;
+  mountWithHoc({
+    setup() {
+      const { errors } = useForm({
+        validateOnMount: true,
+        validationSchema: yup.object({
+          users: yup.array().of(
+            yup.object({
+              name: yup.string().required(),
+            })
+          ),
+        }),
+        initialValues: {
+          users: [{ name: '' }],
+        },
+      });
+
+      fields = useFieldArray<{ name: string }>('users').fields;
+
+      return {
+        fields,
+        errors,
+      };
+    },
+    template: `
+      <p>{{ fields[0].value.name }}</p>
+      <span>{{ errors }}</span>
+    `,
+  });
+
+  await flushPromises();
+  expect(document.querySelector('p')?.innerHTML).toBe('');
+  expect(document.querySelector('span')?.innerHTML).toBeTruthy();
+
+  const item = fields.value[0];
+  item.value.name = 'test';
+
+  await flushPromises();
+  expect(document.querySelector('p')?.innerHTML).toBe('test');
+  expect(document.querySelector('span')?.innerHTML).toBe('{}');
 });
 
 test('warns when updating a no-longer existing item', async () => {
