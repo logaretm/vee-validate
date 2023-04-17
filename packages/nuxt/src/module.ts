70760b935a38d6c871a19f26bfc0b07e8478f659
Abdelrahman Awad <logaretm1@gmail.com>
2023-04-17 02:48:01 +0200

    feat: expose toTyped schema and show helpful logs to the user

--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -1,5 +1,10 @@
 import { defineNuxtModule, addComponent, addImports } from '@nuxt/kit';
 import type { NuxtModule } from '@nuxt/schema';
+import { isPackageExists } from 'local-pkg';
+
+interface VeeValidateModuleOptions {
+  autoImports?: boolean;
+}
 
 const components = ['Field', 'Form', 'ErrorMessage', 'FieldArray'];
 
@@ -25,26 +30,96 @@ const composables = [
   'useValidateForm',
 ];
 
-export default defineNuxtModule({
+export default defineNuxtModule<VeeValidateModuleOptions>({
   meta: {
     name: 'vee-valiate',
     configKey: 'veeValidate',
   },
-  setup(options, nuxt) {
-    composables.forEach(composable => {
-      addImports({
-        name: composable,
-        as: composable,
-        from: 'vee-validate',
-      });
-    });
-
-    components.forEach(component => {
-      addComponent({
-        name: component,
-        export: component,
-        filePath: 'vee-validate',
-      });
-    });
+  defaults: {
+    autoImports: true,
   },
-}) as NuxtModule;
+  setup(options, nuxt) {
+    if (options.autoImports) {
+      composables.forEach(composable => {
+        addImports({
+          name: composable,
+          as: composable,
+          from: 'vee-validate',
+        });
+      });
+
+      components.forEach(component => {
+        addComponent({
+          name: component,
+          export: component,
+          filePath: 'vee-validate',
+        });
+      });
+    }
+
+    const usingYup = checkForYup(options);
+    if (!usingYup) {
+      checkForZod(options);
+    }
+  },
+}) as NuxtModule<VeeValidateModuleOptions>;
+
+function checkForZod(options: VeeValidateModuleOptions) {
+  if (isPackageExists('zod') && !isPackageExists('@vee-validate/zod')) {
+    console.log(
+      'You seem to be using zod, but you have not installed @vee-validate/zod. Please install it to use zod with vee-validate.'
+    );
+    return true;
+  }
+
+  if (isPackageExists('@vee-validate/zod') && !isPackageExists('zod')) {
+    console.log(
+      'You seem to be using @vee-validate/zod, but you have not installed zod. Please install it to use zod with vee-validate.'
+    );
+    return true;
+  }
+
+  if (isPackageExists('@vee-validate/zod') && isPackageExists('zod')) {
+    if (options.autoImports) {
+      addImports({
+        name: 'toTypedSchema',
+        as: 'toTypedSchema',
+        from: '@vee-validate/zod',
+      });
+    }
+
+    return true;
+  }
+
+  return false;
+}
+
+function checkForYup(options: VeeValidateModuleOptions) {
+  if (isPackageExists('yup') && !isPackageExists('@vee-validate/yup')) {
+    console.log(
+      'You seem to be using yup, but you have not installed @vee-validate/yup. Please install it to use yup with vee-validate.'
+    );
+    return true;
+  }
+
+  if (isPackageExists('@vee-validate/yup') && !isPackageExists('yup')) {
+    console.log(
+      'You seem to be using @vee-validate/yup, but you have not installed yup. Please install it to use yup with vee-validate.'
+    );
+    return true;
+  }
+
+  if (isPackageExists('@vee-validate/yup') && isPackageExists('yup')) {
+    if (options.autoImports) {
+      addImports({
+        name: 'toTypedSchema',
+        as: 'toTypedSchema',
+        from: '@vee-validate/yup',
+      });
+    }
+
+    return true;
+  }
+
+  return false;
+}
