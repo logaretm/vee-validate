8eebfc6e38629e4990af70bf3f699af7bdca80ff
Niklas Buchfink <59048346+NiklasBuchfink@users.noreply.github.com>
2023-05-08 11:34:31 +0200

    Update inlang.config.js

--- inlang.config.js
+++ inlang.config.js
@@ -1,28 +1,17 @@
-// @ts-check
-
-/**
- * @type { import("@inlang/core/config").DefineConfig }
- */
 export async function defineConfig(env) {
-  const plugin = await env.$import(
-    "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1/dist/index.js"
-  );
+	const { default: pluginJson } = await env.$import(
+		'https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@2/dist/index.js'
+	);
 
-  const { standardLintRules } = await env.$import(
-    "https://cdn.jsdelivr.net/gh/inlang/standard-lint-rules@1/dist/index.js"
-  );
+	const { default: standardLintRules } = await env.$import(
+		'https://cdn.jsdelivr.net/gh/inlang/standard-lint-rules@2/dist/index.js'
+	);
 
-  const pluginConfig = {
-    pathPattern: "./packages/i18n/src/locale/{language}.json",
-  };
-
-  return {
-    referenceLanguage: "en",
-    languages: await plugin.getLanguages({ ...env, pluginConfig }),
-    readResources: (args) => plugin.readResources({ ...args, ...env, pluginConfig }),
-    writeResources: (args) => plugin.writeResources({ ...args, ...env, pluginConfig }),
-    lint: {
-      rules: [standardLintRules()],
-    },
-  };
+	return {
+		referenceLanguage: 'en',
+		plugins: [pluginJson({ 
+			pathPattern: './packages/i18n/src/locale/{language}.json',
+			variableReferencePattern: ["{", "}"]
+		}), standardLintRules()]
+	};
 }
