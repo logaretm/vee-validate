a54a073064cf56e3d395c35a494d29a9c13d065e
Niklas Buchfink <59048346+NiklasBuchfink@users.noreply.github.com>
2023-06-08 10:11:25 +0200

    add `inlang` to make the contribution of translations easier (#4249)

---
+++ inlang.config.js
@@ -0,0 +1,17 @@
+export async function defineConfig(env) {
+	const { default: pluginJson } = await env.$import(
+		'https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@2/dist/index.js'
+	);
+
+	const { default: standardLintRules } = await env.$import(
+		'https://cdn.jsdelivr.net/gh/inlang/standard-lint-rules@2/dist/index.js'
+	);
+
+	return {
+		referenceLanguage: 'en',
+		plugins: [pluginJson({ 
+			pathPattern: './packages/i18n/src/locale/{language}.json',
+			variableReferencePattern: ["{", "}"]
+		}), standardLintRules()]
+	};
+}
