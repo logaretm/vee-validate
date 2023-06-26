740de5cf5aad26dce14353072b3e423e823d1748
Abdelrahman Awad <logaretm1@gmail.com>
2023-06-26 06:00:13 +0300

    docs: added some utility to examples

--- docs/src/components/Repl.vue
+++ docs/src/components/Repl.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts" async>
-import { version } from 'vue';
+import { ref, version } from 'vue';
+import { useFullscreen } from '@vueuse/core';
 import { Repl, ReplStore } from '@vue/repl';
+import StackBlitzSdk from '@stackblitz/sdk';
 // @ts-ignore
 import CodeMirror from '@vue/repl/codemirror-editor';
 import '@vue/repl/style.css';
+import { getViteProjectConfig } from '@/utils/examples';
 
 const props = defineProps<{
   files: Record<string, string>;
 }>();
+
+const containerRef = ref<HTMLElement>();
+const { isFullscreen, toggle: toggleFullscreen } = useFullscreen(containerRef);
 
 const files = await Promise.all(
   Object.entries(props.files).map(async ([filename, componentName]) => {
@@ -41,22 +47,84 @@ store.setImportMap({
 
 // use a specific version of Vue
 store.setVueVersion(version);
+
+function onForkClick() {
+  const exampleFiles = files.reduce((acc, file) => {
+    acc[file[0]] = file[1];
+
+    return acc;
+  }, {} as Record<string, string>);
+
+  StackBlitzSdk.openProject(getViteProjectConfig(exampleFiles), {
+    newWindow: true,
+    startScript: 'dev',
+    openFile: 'src/App.vue',
+  });
+}
 </script>
 
 <template>
-  <Repl
-    :store="store"
-    :editor="CodeMirror"
-    :showCompileOutput="false"
-    :ssr="false"
-    :showImportMap="false"
-    :class="{ 'no-files': files.length <= 1 }"
-  />
+  <div
+    class="flex flex-col border border-gray-300 dark:border-emerald-500 border-opacity-80 rounded-md shadow-sm overflow-hidden"
+    ref="containerRef"
+  >
+    <div class="bg-gray-700 w-full flex items-center justify-end py-1.5 px-2">
+      <button
+        type="button"
+        class="ml-1 p-1 hover:bg-white hover:bg-opacity-10 rounded"
+        title="Go full screen"
+        @click="toggleFullscreen"
+      >
+        <svg
+          class="w-4 h-4 text-white"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+        >
+          <path
+            v-if="!isFullscreen"
+            d="M13.28 7.78l3.22-3.22v2.69a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.69l-3.22 3.22a.75.75 0 001.06 1.06zM2 17.25v-4.5a.75.75 0 011.5 0v2.69l3.22-3.22a.75.75 0 011.06 1.06L4.56 16.5h2.69a.75.75 0 010 1.5h-4.5a.747.747 0 01-.75-.75zM12.22 13.28l3.22 3.22h-2.69a.75.75 0 000 1.5h4.5a.747.747 0 00.75-.75v-4.5a.75.75 0 00-1.5 0v2.69l-3.22-3.22a.75.75 0 10-1.06 1.06zM3.5 4.56l3.22 3.22a.75.75 0 001.06-1.06L4.56 3.5h2.69a.75.75 0 000-1.5h-4.5a.75.75 0 00-.75.75v4.5a.75.75 0 001.5 0V4.56z"
+          ></path>
+          <path
+            v-else
+            d="M3.28 2.22a.75.75 0 00-1.06 1.06L5.44 6.5H2.75a.75.75 0 000 1.5h4.5A.75.75 0 008 7.25v-4.5a.75.75 0 00-1.5 0v2.69L3.28 2.22zM13.5 2.75a.75.75 0 00-1.5 0v4.5c0 .414.336.75.75.75h4.5a.75.75 0 000-1.5h-2.69l3.22-3.22a.75.75 0 00-1.06-1.06L13.5 5.44V2.75zM3.28 17.78l3.22-3.22v2.69a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.69l-3.22 3.22a.75.75 0 101.06 1.06zM13.5 14.56l3.22 3.22a.75.75 0 101.06-1.06l-3.22-3.22h2.69a.75.75 0 000-1.5h-4.5a.75.75 0 00-.75.75v4.5a.75.75 0 001.5 0v-2.69z"
+          ></path>
+        </svg>
+      </button>
+
+      <button
+        type="button"
+        class="ml-1 p-1 hover:bg-white hover:bg-opacity-10 rounded"
+        title="Fork in StackBlitz"
+        @click="onForkClick"
+      >
+        <svg
+          class="w-4 h-4 text-white"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+        >
+          <path
+            d="M11.983 1.907a.75.75 0 00-1.292-.657l-8.5 9.5A.75.75 0 002.75 12h6.572l-1.305 6.093a.75.75 0 001.292.657l8.5-9.5A.75.75 0 0017.25 8h-6.572l1.305-6.093z"
+          ></path>
+        </svg>
+      </button>
+    </div>
+    <Repl
+      :store="store"
+      :editor="CodeMirror"
+      :showCompileOutput="false"
+      :ssr="false"
+      :showImportMap="false"
+      :class="{ 'no-files': files.length <= 1, 'is-fullscreen': isFullscreen }"
+    />
+  </div>
 </template>
 
 <style lang="postcss" scoped>
 .vue-repl {
-  @apply border border-gray-300 dark:border-emerald-500 border-opacity-80 rounded-md shadow-sm;
   --bg: #e8e8e8 !important;
   --bg-soft: #e8e8e8 !important;
   --border: hsl(0 0% 74%) !important;
@@ -123,6 +191,12 @@ store.setVueVersion(version);
 
   &:deep(.CodeMirror-scroll) {
     max-height: 500px;
+  }
+
+  &.is-fullscreen {
+    &:deep(.CodeMirror-scroll) {
+      max-height: unset;
+    }
   }
 }
 
