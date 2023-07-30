8edc3975038c67e8c503949fd1da92e12e5e5f55
Abdelrahman Awad <logaretm1@gmail.com>
2023-07-31 02:51:52 +0300

    docs: added valibot to field validation

---
+++ docs/src/components/examples/CompositionInputFieldValibot.vue
@@ -0,0 +1,12 @@
+<template>
+  <input type="text" v-model="value" />
+  {{ errorMessage }}
+</template>
+
+<script setup>
+import { useField } from 'vee-validate';
+import { toTypedSchema } from '@vee-validate/valibot';
+import { string, email, minLength } from 'valibot';
+
+const { value, errorMessage } = useField('email', toTypedSchema(string([minLength(1, 'Required'), email()])));
+</script>
