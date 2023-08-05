b3d881324f077895b46d6d0fd8bf26ee22c55e2d
Abdelrahman Awad <logaretm1@gmail.com>
2023-08-05 17:05:25 +0300

    docs: added section on checkboxes

---
+++ docs/src/components/examples/CompositionCustomFieldCheckbox.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useForm } from 'vee-validate';
+import * as yup from 'yup';
+import CustomCheckbox from './CustomCheckbox.vue';
+
+const { values, errors } = useForm({
+  validationSchema: yup.object({
+    drinks: yup.array().of(yup.string()).min(1, 'Please select at least 1 drink').required(),
+  }),
+});
+</script>
+
+<template>
+  <CustomCheckbox name="drinks" checked-value="coffee" /> Coffee
+
+  <CustomCheckbox name="drinks" checked-value="tea" /> Tea
+
+  <CustomCheckbox name="drinks" checked-value="coke" /> Coke
+
+  <pre>values: {{ values }}</pre>
+  <pre>errors: {{ errors }}</pre>
+</template>
---
+++ docs/src/components/examples/CustomCheckboxInputBasic.vue
@@ -0,0 +1,19 @@
+<template>
+  <input :value="checkedValue" :checked="checked" @change="handleChange" type="checkbox" />
+</template>
+
+<script setup>
+import { useField } from 'vee-validate';
+
+const props = defineProps({
+  name: String,
+  checkedValue: String,
+});
+
+// The `name` is returned in a function because we want to make sure it stays reactive
+// If the name changes you want `useField` to be able to pick it up
+const { handleChange, checked } = useField(() => props.name, undefined, {
+  type: 'checkbox',
+  checkedValue: props.checkedValue,
+});
+</script>
