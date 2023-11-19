ed9d7da098ab8120f49859dcc7f8ad4bc56400cc
Abdelrahman Awad <logaretm1@gmail.com>
2023-11-19 13:23:39 +0200

    chore: upgrade dependencies

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-leading-blank': [1, 'always'],
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig, configDefaults } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     setupFiles: ['./vitest.setup.ts'],
     environment: 'jsdom',
     globals: true,
     exclude: ['docs/*', ...configDefaults.exclude],
-    alias: [{ find: /@\/(.+)/, replacement: './packages/$1/src' }],
     coverage: {
       exclude: ['**/*/devtools.ts', 'packages/**/dist/**', 'docs/*', ...(configDefaults.coverage.exclude || [])],
     },
