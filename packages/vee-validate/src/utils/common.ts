9750c9567fd3cecd7dae4816ac956c6b4d5e7dba
Abdelrahman Awad <logaretm1@gmail.com>
2023-11-25 14:57:54 +0200

    fix: remove unused line

--- packages/vee-validate/src/utils/common.ts
+++ packages/vee-validate/src/utils/common.ts
@@ -362,24 +362,20 @@ export function debounceNextTick<
 
 // https://github.com/vueuse/vueuse/blob/main/packages/shared/makeDestructurable/index.ts
 export function makeDestructurable<T extends Record<string, unknown>, A extends readonly any[]>(obj: T, arr: A): T & A {
-  if (typeof Symbol !== 'undefined') {
-    const clone = { ...obj };
+  const clone = { ...obj };
 
-    Object.defineProperty(clone, Symbol.iterator, {
-      enumerable: false,
-      value() {
-        let index = 0;
-        return {
-          next: () => ({
-            value: arr[index++],
-            done: index > arr.length,
-          }),
-        };
-      },
-    });
+  Object.defineProperty(clone, Symbol.iterator, {
+    enumerable: false,
+    value() {
+      let index = 0;
+      return {
+        next: () => ({
+          value: arr[index++],
+          done: index > arr.length,
+        }),
+      };
+    },
+  });
 
-    return clone as T & A;
-  } else {
-    return Object.assign([...arr], obj) as unknown as T & A;
-  }
+  return clone as T & A;
 }
