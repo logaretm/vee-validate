641b2ac29d49449546d15761061f2b0a51ddea59
Abdelrahman Awad <logaretm1@gmail.com>
2024-02-28 14:27:33 +0200

    docs: switch to ethical ads

--- docs/.vuepress/theme/components/Ad.vue
+++ docs/.vuepress/theme/components/Ad.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="ad"></div>
+  <div id="ad">
+    <div data-ea-publisher="vee-validatelogaretmcom" data-ea-type="image"></div>
+  </div>
 </template>
 
 <script>
@@ -10,7 +12,7 @@ export default {
       if (
         to.path !== from.path &&
         // Fix duplicated ads.
-        this.$el.querySelector('#carbonads')
+        this.$el.querySelector('#ad')
       ) {
         this.$el.innerHTML = '';
         this.load();
@@ -23,9 +25,7 @@ export default {
   methods: {
     load() {
       const script = document.createElement('script');
-      script.src = '//cdn.carbonads.com/carbon.js?serve=CE7DKKQ7&placement=vee-validatelogaretmcom';
-      script.id = '_carbonads_js';
-
+      script.src = 'https://media.ethicalads.io/media/client/ethicalads.min.js';
       this.$el.appendChild(script);
     }
   }
@@ -34,96 +34,6 @@ export default {
 
 <style lang="stylus">
 #ad
-  width: 300px
-  position: fixed
-  z-index: 19
   bottom: 10px
   right: 10px
-  background-color: #fff
-  border-radius: 3px
-  font-size: 13px
-
-  #carbonads
-    display: block
-    overflow: hidden
-    padding: 10px
-    box-shadow: 0 1px 3px hsla(0, 0%, 0%, 0.05)
-    border-radius: 4px
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', Helvetica, Arial, sans-serif
-    line-height: 1.5
-    max-width: 300px
-    font-size: 12px
-    background-color: #fff
-
-  #carbonads a
-    text-decoration: none
-
-  #carbonads span
-    position: relative
-    display: block
-    overflow: hidden
-
-  .carbon-img
-    float: left
-    margin-right: 1em
-
-  .carbon-img img
-    display: block
-
-  .carbon-text
-    display: block
-    float: left
-    max-width: calc(100% - 130px - 1em)
-    text-align: left
-    color: #637381
-    padding-bottom: 12px
-
-  .carbon-poweredby
-    position: absolute
-    left: 142px
-    bottom: 0
-    display: block
-    font-size: 10px
-    color: #4b6169
-    font-weight: 500
-    text-transform: uppercase
-    line-height: 1
-    letter-spacing: 1px
-
-  .carbon-wrap
-    overflow: auto
-
-@media screen and (max-width: 1500px) and (min-width: 1301px)
-  #ad
-    width: 150px
-
-    #carbonads
-      max-width: 150px
-
-    .carbon-img
-      margin: 0
-
-    #carbonads, .carbon-wrap
-      display: flex
-      flex-direction: column
-
-    .carbon-text
-      margin-top: 10px
-      max-width: 150px
-
-    .carbon-poweredby
-      position: static
-      margin-top: 10px
-
-@media screen and (max-width: 1300px)
-  #ad
-    margin-top: 10rem
-    margin-left: 0;
-    z-index: 1
-    position: relative
-    padding: 0
-    bottom: 0
-    right: 0
-    float: right
-    padding: 0 0 20px 30px
 </style>
