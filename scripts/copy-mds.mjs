8739ae9a2a039b84a0b304d21ec5c2f25e6dad6b
Abdelrahman Awad <logaretm1@gmail.com>
2025-06-07 16:10:48 +0300

    chore: fix commands

--- scripts/copy-mds.mjs
+++ scripts/copy-mds.mjs
@@ -6,11 +6,14 @@ import chalk from 'chalk';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-fs.copyFileSync(path.resolve(__dirname, '../README.md'), path.resolve(__dirname, '../packages/core/README.md'));
-console.log(chalk.green('📄 README.md copied to packages/core'));
+fs.copyFileSync(path.resolve(__dirname, '../README.md'), path.resolve(__dirname, '../packages/vee-validate/README.md'));
+console.log(chalk.green('📄 README.md copied to packages/vee-validate'));
 
-fs.copyFileSync(path.resolve(__dirname, '../LICENSE'), path.resolve(__dirname, '../packages/core/LICENSE'));
-console.log(chalk.green('📄 LICENSE copied to packages/core'));
+fs.copyFileSync(path.resolve(__dirname, '../LICENSE'), path.resolve(__dirname, '../packages/vee-validate/LICENSE'));
+console.log(chalk.green('📄 LICENSE copied to packages/vee-validate'));
 
-fs.copyFileSync(path.resolve(__dirname, '../packages/core/CHANGELOG.md'), path.resolve(__dirname, '../CHANGELOG.md'));
+fs.copyFileSync(
+  path.resolve(__dirname, '../packages/vee-validate/CHANGELOG.md'),
+  path.resolve(__dirname, '../CHANGELOG.md'),
+);
 console.log(chalk.green('📄 CHANGELOG.md copied to root'));
