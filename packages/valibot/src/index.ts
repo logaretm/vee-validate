3a9ef56529b4db8152254283248cde80772f6fab
Abdelrahman Awad <logaretm1@gmail.com>
2025-08-02 18:03:50 +0300

    feat: kill old packages

--- packages/valibot/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { StandardSchemaV1 } from '@standard-schema/spec';
-
-/**
- * Transforms a Zod object schema to Yup's schema
- * @deprecated No longer needed, use the valibot's schema directly.
- */
-export function toTypedSchema<TSchema extends StandardSchemaV1>(schema: TSchema): StandardSchemaV1 {
-  return schema;
-}
